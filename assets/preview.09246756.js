2e6b989ede82e0c0e06c694eb5b3bddc8f8c42da
micheal-parks <>
2022-06-28 15:23:52 +0000

    Deploying to gh-pages from @ viamrobotics/prime@054ad412429f1b65f9c5711e85fd072683b73bc1 🚀

---
+++ assets/preview.09246756.js
@@ -0,0 +1,2 @@
+const e={actions:{argTypesRegex:"^on[A-Z].*"},controls:{expanded:!0,matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{source:{state:"open"}},inlineStories:!0};export{e as parameters};
+//# sourceMappingURL=preview.09246756.js.map
---
+++ assets/slider.stories.f0a8a871.js
@@ -0,0 +1,54 @@
+import"./index.5c7752e3.js";import{c as e,A as c,M as y,C as u,S as o}from"./Props.43cdf6c7.js";import"./iframe.8cc0bb81.js";const $={},v="wrapper";function d({components:a,...n}){return e(v,{...$,...n,components:a,mdxType:"MDXLayout"},e(y,{title:"Elements/Slider",argTypes:{min:{description:"The minimum value",control:"number",defaultValue:"",table:{defaultValue:{summary:""}}},max:{description:"The maxiumum value",control:"number",defaultValue:"",table:{defaultValue:{summary:""}}}},mdxType:"Meta"}),e("h1",null,"Radio"),e("p",null,"For user triggered actions"),e(u,{mdxType:"Canvas"},e(o,{name:"Slider",args:{min:0,max:100,step:10},mdxType:"Story"},({min:t,max:m,step:r})=>`
+      <v-slider
+        min="${t}"
+        max="${m}"
+        step="${r}"
+      />
+    `)),e(u,{mdxType:"Canvas"},e(o,{name:"Multivalue",args:{min:0,max:100,start:10,end:90,step:10},mdxType:"Story"},({min:t,max:m,step:r,start:p,end:x})=>`
+      <v-slider
+        min="${t}"
+        max="${m}"
+        step="${r}"
+        start="${p}"
+        end="${x}"
+      />
+    `)))}d.isMDXComponent=!0;const l=({min:a,max:n,step:t})=>`
+      <v-slider
+        min="${a}"
+        max="${n}"
+        step="${t}"
+      />
+    `;l.storyName="Slider";l.args={min:0,max:100,step:10};l.parameters={storySource:{source:`({
+  min,
+  max,
+  step
+}) => \`
+      <v-slider
+        min="\${min}"
+        max="\${max}"
+        step="\${step}"
+      />
+    \``}};const i=({min:a,max:n,step:t,start:m,end:r})=>`
+      <v-slider
+        min="${a}"
+        max="${n}"
+        step="${t}"
+        start="${m}"
+        end="${r}"
+      />
+    `;i.storyName="Multivalue";i.args={min:0,max:100,start:10,end:90,step:10};i.parameters={storySource:{source:`({
+  min,
+  max,
+  step,
+  start,
+  end
+}) => \`
+      <v-slider
+        min="\${min}"
+        max="\${max}"
+        step="\${step}"
+        start="\${start}"
+        end="\${end}"
+      />
+    \``}};const s={title:"Elements/Slider",argTypes:{min:{description:"The minimum value",control:"number",defaultValue:"",table:{defaultValue:{summary:""}}},max:{description:"The maxiumum value",control:"number",defaultValue:"",table:{defaultValue:{summary:""}}}},includeStories:["slider","multivalue"]},S={Slider:"slider",Multivalue:"multivalue"};s.parameters=s.parameters||{};s.parameters.docs={...s.parameters.docs||{},page:()=>e(c,{mdxStoryNameToKey:S,mdxComponentAnnotations:s},e(d,null))};export{s as default,i as multivalue,l as slider};
+//# sourceMappingURL=slider.stories.f0a8a871.js.map
