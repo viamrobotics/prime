70ef10bfcc848a139e427e992422fe3a2709e2b7
Anjali Nainani <anjali.nainani@gmail.com>
2022-06-28 13:53:57 -0400

    removes unneeded comments

--- src/elements/badge/index.svelte
+++ src/elements/badge/index.svelte
@@ -17,7 +17,6 @@ export let variant: Variants = 'green'
 
 </script>
 
-<!-- Add padding and font-size per MP -->
 <div class={cx('inline-flex gap-3 rounded-full px-3 py-0.5 text-sm', {
   'text-green-900 bg-green-200': variant === 'green',
   'text-orange-900 bg-orange-200': variant === 'orange',
