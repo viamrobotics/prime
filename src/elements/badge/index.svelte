20c1b130b12f9b164ee9a80107f4210d89cf6c38
Anjali Nainani <anjali.nainani@gmail.com>
2022-06-28 13:45:56 -0400

    adds badge component to prime

---
+++ src/elements/badge/index.svelte
@@ -0,0 +1,28 @@
+<svelte:options
+  immutable={true}
+  tag='v-badge'
+/>
+
+<link rel='stylesheet' href='/prime.css' />
+
+<script lang='ts'>
+
+import cx from 'classnames'
+
+export let label = ''
+
+type Variants = 'green' | 'orange' | 'red'
+
+export let variant: Variants = 'green'
+
+</script>
+
+<!-- Add padding and font-size per MP -->
+<div class={cx('inline-flex gap-3 rounded-full px-3 py-0.5 text-sm', {
+  'text-green-900 bg-green-200': variant === 'green',
+  'text-orange-900 bg-orange-200': variant === 'orange',
+  'text-red-900 bg-red-200': variant === 'red'
+})}>
+  <small>{ label }</small>
+</div>
+
