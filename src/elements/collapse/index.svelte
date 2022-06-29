d8689c846f6da2d20b6337666c8cb2a922b3fe0a
Anjali Nainani <anjali.nainani@gmail.com>
2022-06-29 18:59:40 -0400

    add collapse component

---
+++ src/elements/collapse/index.svelte
@@ -0,0 +1,43 @@
+<svelte:options
+  immutable={true}
+  tag='v-collapse'
+/>
+
+<link rel='stylesheet' href='/prime.css' />
+
+<script lang='ts'>
+
+export let title = ''
+export let content = ''
+
+</script>
+
+<div class="relative w-full overflow-hidden">
+  <input 
+    type="checkbox"
+    class="absolute w-full h-12 opacity-0 cursor-pointer peer">
+
+  <div class="border border-black bg-white h-12 w-full pl-5 pr-5 flex items-center">
+    <p class="text-black">{title}</p>
+  </div>
+
+  <div class="absolute top-3 right-3 transition-transform duration-500 rotate-0 peer-checked:rotate-180">
+    <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    stroke-linejoin="round"
+    stroke-linecap="round"
+    fill="none"
+  >
+    <polyline points="6 9 12 15 18 9" />
+  </svg>
+  </div>
+
+  <div class="bg-white overflow-hidden pl-5 pr-5 pt-2 transition-all duration-500 max-h-0 peer-checked:max-h-fit">
+    <p class="text-black">{content}</p>
+  </div>
+</div>  
+
+
