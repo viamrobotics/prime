95d5fa691ac53528923014fee113617244240e67
Anjali Nainani <anjali.nainani@gmail.com>
2022-07-06 16:51:45 -0400

    creates tooltip in prime

---
+++ src/elements/tooltip/index.svelte
@@ -0,0 +1,32 @@
+<svelte:options immutable={true} tag='v-tooltip' />
+
+<script lang='ts'>
+
+import cx from 'classnames'
+import { addStyles } from '../../lib/index'
+
+export let text = ''
+
+type Variants = 'top' | 'bottom' | 'right' | 'left'
+// type className = string
+
+export let variant: Variants = 'top'
+
+addStyles()
+
+</script>
+
+<!-- :class="`relative inline-block ${className} group`" -->
+
+<div class='relative inline-block ml-2 group'>
+  <slot />
+  <span 
+    class={cx('invisible bg-white text-black text-left p-3 border absolute z-10 min-w-[200px] group-hover:visible after:absolute after:-ml-4 after:border-8 after:border-solid after:border-transparent', {
+      'bottom-[125%] left-[-80%] after:border-t-black after:top-[100%] after:left-[13.5%]' : variant === 'top',
+      'top-[125%] left-[-80%] after:border-b-black after:bottom-[100%] after:left-[13.5%]' : variant === 'bottom',
+      'left-[170%] after:border-r-black after:bottom-[83%] after:left-[0%]' : variant === 'right',
+      'right-[165%] after:border-l-black after:bottom-[83%] after:right-[-8%]' : variant === 'left',
+    })}>
+    {{ text }}
+  </span>
+</div>
