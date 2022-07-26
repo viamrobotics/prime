2004055f85d4e1f1c1228d6ebd575804e601039d
Steven Balasta <sbal13@gmail.com>
2022-07-26 12:19:14 -0400

    finalize tooltip without arrow

--- src/elements/tooltip/index.svelte
+++ src/elements/tooltip/index.svelte
@@ -2,8 +2,7 @@
 
 <script lang='ts'>
 
-import cx from 'classnames'
-import { computePosition, flip, shift } from '@floating-ui/dom';
+import { computePosition, flip, shift, offset } from '@floating-ui/dom';
 
 import { addStyles } from '../../lib/index'
 
@@ -11,30 +10,46 @@ type Locations = 'top' | 'bottom' | 'right' | 'left'
 
 export let text = ''
 export let location: Locations = 'top'
-let target: HTMLElement
+let container: HTMLElement
 let tooltip: HTMLElement
 
-$: {
-    target && tooltip && computePosition(target, tooltip, {
-      placement: location,
-      middleware: [flip(), shift()]
-    }).then(({x, y}) => {
-      Object.assign(tooltip.style, {
-        left: `${x}px`,
-        top: `${y}px`,
-      });
-    });
+let invisible = true
+
+let x = 0
+let y = 0
+
+const recalculateStyle = async () => {
+  const position = await computePosition(container, tooltip, {
+    placement: location,
+    middleware: [flip(), shift({ padding: 5 }), offset(10)]
+  })
+
+  x = position.x
+  y = position.y
+}
+
+const handleMouseEnter = async () => {
+  await recalculateStyle()
+  invisible = false
+}
+
+const handleMouseLeave = () => {
+  invisible = true
 }
 
 addStyles()
 
 </script>
 
-<div class="relative">
-  <div bind:this={target} aria-describedby="tooltip">
-    <slot />
-  </div>
-  <span
+<div
+  bind:this={container}
+  class="relative"
+  aria-describedby="tooltip"
+  on:mouseenter={handleMouseEnter}
+  on:mouseleave={handleMouseLeave}
+>
+  <slot />
+  <div
     bind:this={tooltip}
     role="tooltip"
     class={`
@@ -48,7 +63,9 @@ addStyles()
       border
       z-10
     `}
+    class:invisible={invisible}
+    style='transform: translate({x}px, {y}px);'
     >
     {text}
-  </span>
+  </div>
 </div>
