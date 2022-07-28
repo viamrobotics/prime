7b98c334464048da7f8c2f77c1f519c2bd0e68d9
Micheal Parks <micheal.parks@outlook.com>
2022-07-28 13:36:20 -0400

    add references to svelte issue

--- src/elements/button/button.ts
+++ src/elements/button/button.ts
@@ -1,5 +1,6 @@
 import './button.svelte'
 
+// https://github.com/sveltejs/svelte/issues/7596
 class Button extends customElements.get('v-button-internal')! {
   static formAssociated = true
 }
--- src/elements/input/input.ts
+++ src/elements/input/input.ts
@@ -1,5 +1,6 @@
 import './input.svelte'
 
+// https://github.com/sveltejs/svelte/issues/7596
 class Input extends customElements.get('v-input-internal')! {
   static formAssociated = true
 }
