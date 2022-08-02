835d1e30a1b0a5c6a5ce325d225c118649e8e1aa
Devin T. Currie <devin@viam.com>
2022-08-02 11:35:08 -0400

    Move code examples to separate files to avoid parsing errors

---
+++ src/stories/assets/javascript-code-example.js
@@ -0,0 +1,5 @@
+export default (`
+function doSomething(thing) { 
+  console.log(thing) 
+}
+`).trim()
---
+++ src/stories/assets/json-code-example.js
@@ -0,0 +1,8 @@
+export default (`
+{ 
+  "foo": "bar",
+  "some": "thing",
+  "yes": true,
+  "count": 10
+}
+`).trim()
