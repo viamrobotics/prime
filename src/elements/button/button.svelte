e468202116ac69db5bc3e6fb7eeadae98f145d40
Devin T. Currie <devin@viam.com>
2022-08-04 16:12:33 -0400

    Add inverse-primary button variant

--- src/elements/button/button.svelte
+++ src/elements/button/button.svelte
@@ -6,7 +6,7 @@ import cx from 'classnames'
 import { get_current_component } from 'svelte/internal'
 import { addStyles } from '../../lib/index'
 
-type Variants = 'primary' | 'danger' | 'outline-danger' | 'success'
+type Variants = 'primary' | 'inverse-primary' | 'danger' | 'outline-danger' | 'success'
 
 export let disabled: 'true' | 'false' = 'false'
 export let type: 'button' | 'submit' | 'reset' = 'button'
@@ -37,6 +37,7 @@ const handleClick = () => {
   class={cx('flex items-center gap-1.5 py-1.5 px-2 text-xs border', {
     'cursor-not-allowed opacity-50 pointer-events-none': disabled === 'true',
     'bg-white border-black': variant === 'primary',
+    'bg-black border-white text-white': variant === 'inverse-primary',
     'bg-red/90 text-white border-red/90': variant === 'danger',
     'bg-green/90 border-green/90 text-white': variant === 'success',
     'bg-white border-red/90 text-red/90': variant === 'outline-danger',
