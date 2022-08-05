f8eadf21f2bab4af7bfa48c659fc16b49dde6134
DTCurrie <>
2022-08-05 17:27:40 +0000

    Deploying to gh-pages from @ viamrobotics/prime@523fdfa4249531f22866210820c1750bc962961c 🚀

---
+++ assets/slider.stories.9fc80c6e.js
@@ -0,0 +1,186 @@
+import"./jsx-runtime.59ebbf32.js";import{c as n,A as b,M as h,C as i,S as l}from"./Props.2a62f1e9.js";import"./iframe.0ed83d26.js";import"./es.map.constructor.f61e0239.js";import"./es.number.to-fixed.07b3f5b4.js";function p(){return p=Object.assign?Object.assign.bind():function(s){for(var r=1;r<arguments.length;r++){var e=arguments[r];for(var a in e)Object.prototype.hasOwnProperty.call(e,a)&&(s[a]=e[a])}return s},p.apply(this,arguments)}const S={},T="wrapper";function f({components:s,...r}){return n(T,p({},S,r,{components:s,mdxType:"MDXLayout"}),n(h,{title:"Elements/Slider",parameters:{actions:{handles:["input","click"]}},argTypes:{min:{description:"The minimum value",control:"number",table:{defaultValue:{summary:0}}},max:{description:"The maxiumum value",control:"number",table:{defaultValue:{summary:100}}},step:{description:"An optional discrete step between values",control:"number",table:{defaultValue:{summary:1}}},value:{description:"An optional starting value",control:"number",table:{defaultValue:{summary:"(min + max) / 2"}}},label:{description:"An optional label",table:{defaultValue:{summary:""}}},suffix:{description:"An optional suffix",table:{defaultValue:{summary:""}}},"on:input":{description:"Event fired when an option is selected"}},mdxType:"Meta"}),n("h1",null,"Slider"),n("p",null,"For user triggered actions"),n(i,{mdxType:"Canvas"},n(l,{name:"Slider",args:{min:0,max:100,step:10},mdxType:"Story"},({min:e,max:a,step:t})=>`
+      <v-slider
+        min='${e}'
+        max='${a}'
+        step='${t}'
+      />
+    `)),n(i,{mdxType:"Canvas"},n(l,{name:"Range",args:{min:0,max:100,start:10,end:90,step:10},mdxType:"Story"},({min:e,max:a,step:t,start:m,end:o})=>`
+      <v-slider
+        min='${e}'
+        max='${a}'
+        step='${t}'
+        start='${m}'
+        end='${o}'
+      />
+    `)),n(i,{mdxType:"Canvas"},n(l,{name:"Min range",args:{range:"min",min:0,max:100,value:30,step:10},mdxType:"Story"},({range:e,min:a,max:t,step:m,value:o})=>`
+      <v-slider
+        range='${e}'
+        max='${t}'
+        step='${m}'
+        value='${o}'
+      />
+    `)),n(i,{mdxType:"Canvas"},n(l,{name:"Max range",args:{range:"max",min:0,max:100,value:70,step:10},mdxType:"Story"},({range:e,min:a,max:t,step:m,value:o})=>`
+      <v-slider
+        range='${e}'
+        max='${t}'
+        step='${m}'
+        value='${o}'
+      />
+    `)),n(i,{mdxType:"Canvas"},n(l,{name:"With suffix",args:{min:0,max:100,step:10,suffix:"ml"},mdxType:"Story"},({min:e,max:a,step:t,suffix:m})=>`
+      <v-slider
+        min='${e}'
+        max='${a}'
+        step='${t}'
+        suffix='${m}'
+      />
+    `)),n(i,{mdxType:"Canvas"},n(l,{name:"With value",args:{min:0,max:100,step:10,value:0},mdxType:"Story"},({value:e,min:a,max:t,step:m})=>`
+      <v-slider
+        value='${e}'
+        min='${a}'
+        max='${t}'
+        step='${m}'
+      />
+    `)),n(i,{mdxType:"Canvas"},n(l,{name:"With label",args:{min:0,max:100,step:10,label:"Soups eaten per day"},mdxType:"Story"},({label:e,min:a,max:t,step:m})=>`
+      <v-slider
+        label='${e}'
+        min='${a}'
+        max='${t}'
+        step='${m}'
+      />
+    `)))}f.isMDXComponent=!0;const x=({min:s,max:r,step:e})=>`
+      <v-slider
+        min='${s}'
+        max='${r}'
+        step='${e}'
+      />
+    `;x.storyName="Slider";x.args={min:0,max:100,step:10};x.parameters={storySource:{source:`({
+  min,
+  max,
+  step
+}) => \`
+      <v-slider
+        min='\${min}'
+        max='\${max}'
+        step='\${step}'
+      />
+    \``}};const d=({min:s,max:r,step:e,start:a,end:t})=>`
+      <v-slider
+        min='${s}'
+        max='${r}'
+        step='${e}'
+        start='${a}'
+        end='${t}'
+      />
+    `;d.storyName="Range";d.args={min:0,max:100,start:10,end:90,step:10};d.parameters={storySource:{source:`({
+  min,
+  max,
+  step,
+  start,
+  end
+}) => \`
+      <v-slider
+        min='\${min}'
+        max='\${max}'
+        step='\${step}'
+        start='\${start}'
+        end='\${end}'
+      />
+    \``}};const $=({range:s,min:r,max:e,step:a,value:t})=>`
+      <v-slider
+        range='${s}'
+        max='${e}'
+        step='${a}'
+        value='${t}'
+      />
+    `;$.storyName="Min range";$.args={range:"min",min:0,max:100,value:30,step:10};$.parameters={storySource:{source:`({
+  range,
+  min,
+  max,
+  step,
+  value
+}) => \`
+      <v-slider
+        range='\${range}'
+        max='\${max}'
+        step='\${step}'
+        value='\${value}'
+      />
+    \``}};const c=({range:s,min:r,max:e,step:a,value:t})=>`
+      <v-slider
+        range='${s}'
+        max='${e}'
+        step='${a}'
+        value='${t}'
+      />
+    `;c.storyName="Max range";c.args={range:"max",min:0,max:100,value:70,step:10};c.parameters={storySource:{source:`({
+  range,
+  min,
+  max,
+  step,
+  value
+}) => \`
+      <v-slider
+        range='\${range}'
+        max='\${max}'
+        step='\${step}'
+        value='\${value}'
+      />
+    \``}};const v=({min:s,max:r,step:e,suffix:a})=>`
+      <v-slider
+        min='${s}'
+        max='${r}'
+        step='${e}'
+        suffix='${a}'
+      />
+    `;v.storyName="With suffix";v.args={min:0,max:100,step:10,suffix:"ml"};v.parameters={storySource:{source:`({
+  min,
+  max,
+  step,
+  suffix
+}) => \`
+      <v-slider
+        min='\${min}'
+        max='\${max}'
+        step='\${step}'
+        suffix='\${suffix}'
+      />
+    \``}};const y=({value:s,min:r,max:e,step:a})=>`
+      <v-slider
+        value='${s}'
+        min='${r}'
+        max='${e}'
+        step='${a}'
+      />
+    `;y.storyName="With value";y.args={min:0,max:100,step:10,value:0};y.parameters={storySource:{source:`({
+  value,
+  min,
+  max,
+  step
+}) => \`
+      <v-slider
+        value='\${value}'
+        min='\${min}'
+        max='\${max}'
+        step='\${step}'
+      />
+    \``}};const g=({label:s,min:r,max:e,step:a})=>`
+      <v-slider
+        label='${s}'
+        min='${r}'
+        max='${e}'
+        step='${a}'
+      />
+    `;g.storyName="With label";g.args={min:0,max:100,step:10,label:"Soups eaten per day"};g.parameters={storySource:{source:`({
+  label,
+  min,
+  max,
+  step
+}) => \`
+      <v-slider
+        label='\${label}'
+        min='\${min}'
+        max='\${max}'
+        step='\${step}'
+      />
+    \``}};const u={title:"Elements/Slider",parameters:{actions:{handles:["input","click"]}},argTypes:{min:{description:"The minimum value",control:"number",table:{defaultValue:{summary:0}}},max:{description:"The maxiumum value",control:"number",table:{defaultValue:{summary:100}}},step:{description:"An optional discrete step between values",control:"number",table:{defaultValue:{summary:1}}},value:{description:"An optional starting value",control:"number",table:{defaultValue:{summary:"(min + max) / 2"}}},label:{description:"An optional label",table:{defaultValue:{summary:""}}},suffix:{description:"An optional suffix",table:{defaultValue:{summary:""}}},"on:input":{description:"Event fired when an option is selected"}},includeStories:["slider","range","minRange","maxRange","withSuffix","withValue","withLabel"]},w={Slider:"slider",Range:"range","Min range":"minRange","Max range":"maxRange","With suffix":"withSuffix","With value":"withValue","With label":"withLabel"};u.parameters=u.parameters||{};u.parameters.docs={...u.parameters.docs||{},page:()=>n(b,{mdxStoryNameToKey:w,mdxComponentAnnotations:u},n(f,null))};const N=["slider","range","minRange","maxRange","withSuffix","withValue","withLabel"];export{N as __namedExportsOrder,u as default,c as maxRange,$ as minRange,d as range,x as slider,g as withLabel,v as withSuffix,y as withValue};
+//# sourceMappingURL=slider.stories.9fc80c6e.js.map
---
+++ assets/tooltip.stories.4d8d9a57.js
@@ -0,0 +1,23 @@
+import"./jsx-runtime.59ebbf32.js";import{c as t,A as s,M as m,C as c,S as d}from"./Props.2a62f1e9.js";import"./iframe.0ed83d26.js";import"./es.map.constructor.f61e0239.js";import"./es.number.to-fixed.07b3f5b4.js";function l(){return l=Object.assign?Object.assign.bind():function(o){for(var e=1;e<arguments.length;e++){var n=arguments[e];for(var i in n)Object.prototype.hasOwnProperty.call(n,i)&&(o[i]=n[i])}return o},l.apply(this,arguments)}const y={},u="wrapper";function r({components:o,...e}){return t(u,l({},y,e,{components:o,mdxType:"MDXLayout"}),t(m,{title:"Elements/Tooltip",argTypes:{location:{description:"The placement of the tooltip/tooltip direction",control:{type:"select"},options:["top","bottom","left","right"],table:{defaultValue:{summary:"top"}}},text:{description:"The message displayed within the tooltip",table:{defaultValue:{summary:""}}}},mdxType:"Meta"}),t("h1",null,"Tooltip"),t("p",null,"Used to display text box when hovering over an element. "),t(c,{mdxType:"Canvas"},t(d,{name:"Tooltip",args:{text:"Test information for the tooltip",location:"top"},mdxType:"Story"},({text:n,location:i})=>`
+    <div style="width: 100%; height: 200px; display: flex; align-items: center; justify-content:center">
+        <v-tooltip text='${n}' location="${i}">
+          <p>Hover here</p>
+        </v-tooltip>
+    <div>
+    `)))}r.isMDXComponent=!0;const p=({text:o,location:e})=>`
+    <div style="width: 100%; height: 200px; display: flex; align-items: center; justify-content:center">
+        <v-tooltip text='${o}' location="${e}">
+          <p>Hover here</p>
+        </v-tooltip>
+    <div>
+    `;p.storyName="Tooltip";p.args={text:"Test information for the tooltip",location:"top"};p.parameters={storySource:{source:`({
+  text,
+  location
+}) => \`
+    <div style="width: 100%; height: 200px; display: flex; align-items: center; justify-content:center">
+        <v-tooltip text='\${text}' location="\${location}">
+          <p>Hover here</p>
+        </v-tooltip>
+    <div>
+    \``}};const a={title:"Elements/Tooltip",argTypes:{location:{description:"The placement of the tooltip/tooltip direction",control:{type:"select"},options:["top","bottom","left","right"],table:{defaultValue:{summary:"top"}}},text:{description:"The message displayed within the tooltip",table:{defaultValue:{summary:""}}}},includeStories:["tooltip"]},h={Tooltip:"tooltip"};a.parameters=a.parameters||{};a.parameters.docs={...a.parameters.docs||{},page:()=>t(s,{mdxStoryNameToKey:h,mdxComponentAnnotations:a},t(r,null))};const b=["tooltip"];export{b as __namedExportsOrder,a as default,p as tooltip};
+//# sourceMappingURL=tooltip.stories.4d8d9a57.js.map
