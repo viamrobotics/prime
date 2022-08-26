191c6c4d12a8ff08b3812016a56d805ee9840f7d
DTCurrie <>
2022-08-26 19:43:21 +0000

    Deploying to gh-pages from @ viamrobotics/prime@44dab2caff76170aa9e22a7ceedbeb1e0923c2ee 🚀

---
+++ assets/badge.stories.bfdfe802.js
@@ -0,0 +1,74 @@
+import"./jsx-runtime.d1b9f538.js";import{c as a,A as v,M as p,C as o,S as s}from"./Props.c55b6364.js";import"./iframe.2892b64b.js";import"./es.map.constructor.45a3d33c.js";import"./es.number.to-fixed.7fc33d5d.js";function g(){return g=Object.assign?Object.assign.bind():function(r){for(var e=1;e<arguments.length;e++){var n=arguments[e];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(r[t]=n[t])}return r},g.apply(this,arguments)}const c={},u="wrapper";function b({components:r,...e}){return a(u,g({},c,e,{components:r,mdxType:"MDXLayout"}),a(p,{title:"Elements/Badge",argTypes:{variant:{description:"The badge color",control:{type:"select"},options:["gray","green","orange","red"],table:{defaultValue:{summary:"gray"}}},label:{description:"The badge status",table:{defaultValue:{summary:""}}}},mdxType:"Meta"}),a("h1",null,"Badge"),a("p",null,"To display status of something"),a(o,{mdxType:"Canvas"},a(s,{name:"gray",args:{variant:"gray",label:"Inactive"},mdxType:"Story"},({label:n,variant:t})=>`
+      <v-badge
+        variant='${t}'
+        label='${n}'
+      />
+    `)),a(o,{mdxType:"Canvas"},a(s,{name:"green",args:{variant:"green",label:"Active"},mdxType:"Story"},({label:n,variant:t})=>`
+      <v-badge
+        variant='${t}'
+        label='${n}'
+      />
+    `)),a(o,{mdxType:"Canvas"},a(s,{name:"orange",args:{variant:"orange",label:"Danger"},mdxType:"Story"},({label:n,variant:t})=>`
+      <v-badge
+        variant='${t}'
+        label='${n}'
+      />
+    `)),a(o,{mdxType:"Canvas"},a(s,{name:"red",args:{variant:"red",label:"Unhealthy"},mdxType:"Story"},({label:n,variant:t})=>`
+      <v-badge
+        variant='${t}'
+        label='${n}'
+      />
+    `)))}b.isMDXComponent=!0;const i=({label:r,variant:e})=>`
+      <v-badge
+        variant='${e}'
+        label='${r}'
+      />
+    `;i.storyName="gray";i.args={variant:"gray",label:"Inactive"};i.parameters={storySource:{source:`({
+  label,
+  variant
+}) => \`
+      <v-badge
+        variant='\${variant}'
+        label='\${label}'
+      />
+    \``}};const d=({label:r,variant:e})=>`
+      <v-badge
+        variant='${e}'
+        label='${r}'
+      />
+    `;d.storyName="green";d.args={variant:"green",label:"Active"};d.parameters={storySource:{source:`({
+  label,
+  variant
+}) => \`
+      <v-badge
+        variant='\${variant}'
+        label='\${label}'
+      />
+    \``}};const m=({label:r,variant:e})=>`
+      <v-badge
+        variant='${e}'
+        label='${r}'
+      />
+    `;m.storyName="orange";m.args={variant:"orange",label:"Danger"};m.parameters={storySource:{source:`({
+  label,
+  variant
+}) => \`
+      <v-badge
+        variant='\${variant}'
+        label='\${label}'
+      />
+    \``}};const y=({label:r,variant:e})=>`
+      <v-badge
+        variant='${e}'
+        label='${r}'
+      />
+    `;y.storyName="red";y.args={variant:"red",label:"Unhealthy"};y.parameters={storySource:{source:`({
+  label,
+  variant
+}) => \`
+      <v-badge
+        variant='\${variant}'
+        label='\${label}'
+      />
+    \``}};const l={title:"Elements/Badge",argTypes:{variant:{description:"The badge color",control:{type:"select"},options:["gray","green","orange","red"],table:{defaultValue:{summary:"gray"}}},label:{description:"The badge status",table:{defaultValue:{summary:""}}}},includeStories:["gray","green","orange","red"]},$={gray:"gray",green:"green",orange:"orange",red:"red"};l.parameters=l.parameters||{};l.parameters.docs={...l.parameters.docs||{},page:()=>a(v,{mdxStoryNameToKey:$,mdxComponentAnnotations:l},a(b,null))};const C=["gray","green","orange","red"];export{C as __namedExportsOrder,l as default,i as gray,d as green,m as orange,y as red};
+//# sourceMappingURL=badge.stories.bfdfe802.js.map
