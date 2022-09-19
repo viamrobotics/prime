9c6e63ce2b55e496dcc15d7539e9dd25722431bd
mrloureed <mrloureed@gmail.com>
2022-09-19 15:12:22 -0400

    add forgotten icon sizes

--- src/elements/icon.svelte
+++ src/elements/icon.svelte
@@ -23,7 +23,7 @@ addStyles();
     'text-lg': size === 'lg',
     'text-xl': size === 'xl',
     'text-2xl': size === '2xl',
-    'text-3xl': size === '2xl',
-    'text-4xl': size === '2xl',
+    'text-3xl': size === '3xl',
+    'text-4xl': size === '4xl',
   })}
 />
