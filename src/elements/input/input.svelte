ecd11b793b8992c3afafb37921d11df1c7cfe3d6
Steven Balasta <sbal13@gmail.com>
2022-10-10 17:32:07 -0400

    only error

--- src/elements/input/input.svelte
+++ src/elements/input/input.svelte
@@ -92,7 +92,6 @@ const increment = (direction: 1 | -1) => {
           'icon-info-outline': state === 'info',
           'icon-error-outline text-orange-400': state === 'warn',
           'icon-error-outline text-red-600': state === 'error',
-          'icon-error-outline text-green-700': state === 'success',
         })} />
       </v-tooltip>
     {/if}
@@ -110,9 +109,7 @@ const increment = (direction: 1 | -1) => {
     class={cx('w-full py-1.5 px-2.5 leading-tight text-xs h-[30px] border border-black outline-none appearance-none', {
       'bg-white': !isDisabled,
       'opacity-50 pointer-events-none bg-gray-200': isDisabled,
-      'border-orange-400 border-2 bg-orange-100': state === 'warn',
-      'border-red-600 border-2 bg-red-100': state === 'error',
-      'border-green-700 border-2 bg-green-100': state === 'success',
+      'border-red-600 border': state === 'error',
     })}
     step={insertStepAttribute ? step : null}
     on:input={handleInput}
@@ -134,9 +131,7 @@ const increment = (direction: 1 | -1) => {
   {/if}
   {#if message}
     <span class={cx('text-xs', {
-      'text-orange-400': state === 'warn',
       'text-red-600': state === 'error',
-      'text-green-700': state === 'success',
     })}>
       {message}
     </span>
