aa63f416fb5fafba367fb38f0ae0401fd2baeddc
mrloureed <mrloureed@gmail.com>
2022-10-17 13:32:42 -0400

    merge

--- src/elements/select/select.svelte
+++ src/elements/select/select.svelte
@@ -109,7 +109,12 @@ const handleEnter = () => {
     input.focus();
 
     if (optionMatch) {
-      value += `${optionMatchText},`;
+      if (value.includes(optionMatchText)) {
+        value = value.replace(`${optionMatchText},`, '');
+      } else {
+        value += `${optionMatchText},`;
+      }
+      searchTerm = '';
     }
   } else {
     if (navigationIndex > -1) {
@@ -122,7 +127,7 @@ const handleEnter = () => {
       }
     }
     if (open) {
-      dispatch(root, 'input', { value });
+      input.blur();
     }
   }
   dispatch(root, 'input', { value });
