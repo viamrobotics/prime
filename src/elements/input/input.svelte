a8770da8b3840261c2cfbb3aceb1507af860477f
Micheal Parks <103450731+micheal-parks@users.noreply.github.com>
2022-11-07 09:53:33 -0500

    Add sliders to number inputs (#120)

--- src/elements/input/input.svelte
+++ src/elements/input/input.svelte
@@ -3,6 +3,7 @@
 <script lang='ts'>
 
 import cx from 'classnames';
+import { tick } from 'svelte';
 import { get_current_component } from 'svelte/internal';
 import { htmlToBoolean } from '../../lib/boolean';
 import { addStyles, dispatch } from '../../lib/index';
@@ -22,6 +23,8 @@ export let label = '';
 export let value = '';
 export let step = '1';
 export let name = '';
+export let min = '-Infinity';
+export let max = '+Infinity';
 export let labelposition: LabelPosition = 'top';
 export let tooltip = '';
 export let state: 'info' | 'warn' | 'error'| 'success' | '' = 'info';
@@ -29,16 +32,18 @@ export let message: '';
 
 let root: HTMLElement;
 let input: HTMLInputElement;
-let stepDecimalDigits: number;
 let isReadonly: boolean;
 let isDisabled: boolean;
 let stepNumber: number;
+let minNumber: number;
+let maxNumber: number;
 let insertStepAttribute: boolean;
 
-$: stepDecimalDigits = String(step).split('.').pop()?.length ?? 0;
 $: isReadonly = htmlToBoolean(readonly, 'readonly');
 $: isDisabled = htmlToBoolean(disabled, 'disabled');
 $: stepNumber = Number.parseFloat(step);
+$: minNumber = Number.parseFloat(min);
+$: maxNumber = Number.parseFloat(max);
 $: insertStepAttribute = type === 'time' || type === 'number';
 
 addStyles();
@@ -53,18 +58,72 @@ const handleInput = (event: Event) => {
   dispatch(root, 'input', { value });
 };
 
-const increment = (direction: 1 | -1) => {
-  const numberValue = Number.parseFloat(value || '0');
-  const currentValueDigits = String(value).split('.').pop()?.length ?? 0;
+let numberDragTooltip: HTMLElement & { recalculateStyle(): void };
+let numberDragCord: HTMLElement;
+let numberDragHead: HTMLElement;
+let isDragging = false;
+let startX = 0;
+let startValue = 0;
 
-  if (type === 'number') {
-    value = input.value = (numberValue + stepNumber * direction).toFixed(Math.max(stepDecimalDigits, currentValueDigits));
-  } else if (type === 'integer') {
-    value = input.value = String(Math.round(numberValue + stepNumber * direction));
+const handleNumberDragMove = (event: PointerEvent) => {
+  const x = event.clientX;
+  const delta = -(startX - x) * stepNumber / 10;
+
+  value = input.value = (startValue + delta).toFixed(type === 'integer' ? 0 : 1);
+
+  const valueNum = Number.parseFloat(value);
+
+  if (valueNum > maxNumber) {
+    value = String(maxNumber);
+    return;
   }
-  
+
+  if (valueNum < minNumber) {
+    value = String(minNumber);
+    return;
+  }
+
+  numberDragHead.style.transform = `translate(${x - 4}px, 0)`;
+
+  numberDragCord.style.cssText = valueNum > startValue ? `
+      left: ${startX}px;
+      right: ${x}px;
+      width: ${x - startX}px;
+    ` : `
+      left: ${x}px;
+      right: ${startX}px;
+      width: ${startX - x}px;
+    `;
+
+  console.log('x:', x, 'startX', startX, 'width:', startX - x);
+
   internals.setFormValue(value);
   dispatch(root, 'input', { value });
+
+  numberDragTooltip.recalculateStyle();
+};
+
+const handleNumberDragUp = () => {
+  isDragging = false;
+
+  window.removeEventListener('pointermove', handleNumberDragMove);
+};
+
+const handleNumberDragDown = async (event: PointerEvent) => {
+  event.preventDefault();
+  event.stopPropagation();
+
+  startX = event.clientX;
+  startValue = Number.parseFloat(value);
+  isDragging = true;
+
+  await tick();
+
+  numberDragHead.style.transform = `translate(${event.clientX - 4}px, 0)`;
+  numberDragTooltip.recalculateStyle();
+
+  window.addEventListener('pointermove', handleNumberDragMove);
+  window.addEventListener('pointerup', handleNumberDragUp, { once: true });
 };
 
 </script>
@@ -106,9 +165,11 @@ const increment = (direction: 1 | -1) => {
     readonly={isReadonly || isDisabled}
     aria-disabled={isDisabled}
     bind:this={input}
-    class={cx('w-full py-1.5 px-2.5 leading-tight text-xs h-[30px] border border-black outline-none appearance-none', {
+    class={cx('w-full py-1.5 pr-2.5 leading-tight text-xs h-[30px] border border-black outline-none appearance-none', {
+      'pl-2.5': type !== 'number' && type !== 'integer',
+      'pl-3': type === 'number' || type === 'integer',
       'bg-white': !isDisabled,
-      'opacity-50 pointer-events-none bg-gray-200': isDisabled,
+      'opacity-50 pointer-events-none bg-gray-200': isDisabled || isDragging,
       'border-red-600 border': state === 'error',
     })}
     step={insertStepAttribute ? step : null}
@@ -116,19 +177,35 @@ const increment = (direction: 1 | -1) => {
   />
 
   {#if type === 'number' || type === 'integer'}
-    <div class='absolute right-0.5 bottom-0 cursor-pointer select-none flex flex-col'>
-      <button
-        on:click={() => increment(+1)}
-        aria-label='Increment up by {stepNumber}'
-        class='icon-chevron-down rotate-180 text-[15px]'
-      />
-      <button
-        on:click={() => increment(-1)}
-        aria-label='Increment down by {stepNumber}'
-        class='icon-chevron-down text-[15px]'
-      />
+    <div
+      class='absolute left-[0.2rem] bottom-[3px] h-[24px] w-1 bg-gray-400 hover:bg-gray-700 cursor-pointer'
+      on:pointerdown={handleNumberDragDown}
+    >
+      {#if isDragging}
+        <div
+          bind:this={numberDragCord}
+          class='fixed h-px bg-gray-400 mt-[calc(13px)] pointer-events-none'
+        />
+        <div
+          bind:this={numberDragHead}
+          class='fixed left-0 w-2 h-[26px] pointer-events-none'
+        >
+          <div class='absolute h-2 w-2 top-[calc(9px)]'>
+            <v-tooltip
+              bind:this={numberDragTooltip}
+              state='visible'
+              minwidth='auto'
+              text={value}
+            >
+              <div class='absolute  h-2 w-2 bg-gray-800 rounded-full ' />
+            </v-tooltip>
+          </div>
+        </div>
+        
+      {/if}
     </div>
   {/if}
+
   {#if message}
     <span class={cx('text-xs', {
       'text-red-600': state === 'error',
