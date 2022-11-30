ca923aaeed432026be2e86f256690d0a234b960b
Alexis Wei <qq_wei369@hotmail.com>
2022-11-30 11:28:09 -0500

    [DATA-800] make v-pill component (#137)

---
+++ src/elements/pill.svelte
@@ -0,0 +1,37 @@
+<svelte:options immutable tag='v-pill' />
+
+<script lang='ts'>
+
+import { dispatcher } from '../lib/dispatch';
+import { addStyles } from '../lib/index';
+import { htmlToBoolean } from '../lib/boolean';
+
+export let value = ''; 
+export let removable = 'true';
+let isRemovable: boolean;
+$: isRemovable = htmlToBoolean(removable, 'removable');
+
+
+const dispatch = dispatcher();
+addStyles();
+
+const handleRemove = () => {
+  dispatch('remove', { value });
+};
+
+</script>
+
+
+<div class='flex items-center max-w-fit gap-1 rounded-xl bg-[#C4C4C4] py-0.5 px-2 text-[10px] hover:bg-gray-300'>
+  <span>
+    {value}
+  </span>
+  {#if isRemovable}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <v-icon 
+      class='cursor-pointer'
+      name='x'
+      on:click={handleRemove}
+    />
+  {/if}
+</div>
