47f84c57b51aefcfd8c058a925fa39998d2fda17
Micheal Parks <micheal.parks@outlook.com>
2022-12-12 17:46:51 -0500

    add collapsible test

--- playground/src/Test.vue
+++ playground/src/Test.vue
@@ -3,8 +3,14 @@
 </script>
 
 <template>
-  <v-multiselect
-    label="Bobbins"
-    options="Jolly logs, Sleeping hogs, Sticky bogs, Fiendish dogs"
-  />
+  <main class="m-3">
+    <v-collapse title="Click to find out!">
+      <p>You found out!</p>
+    </v-collapse>
+
+    <v-multiselect
+      label="Bobbins"
+      options="Jolly logs, Sleeping hogs, Sticky bogs, Fiendish dogs"
+    />
+  </main>
 </template>
--- playground/src/main.ts
+++ playground/src/main.ts
@@ -4,3 +4,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
