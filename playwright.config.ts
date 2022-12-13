077b2b37bf18175c540b2b50df3f1027acb23b6c
Micheal Parks <micheal.parks@outlook.com>
2022-12-13 11:30:06 -0500

    no sudo

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -99,7 +99,7 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'cd playground && sudo npm start',
+    command: 'cd playground && npm start',
     port: 1000,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
