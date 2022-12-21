2f17f8cc75a38c24e39b509f0e899826cff101a6
Micheal Parks <103450731+micheal-parks@users.noreply.github.com>
2022-12-21 14:17:00 +0100

    add exports (#162)

--- scripts/util.js
+++ scripts/util.js
@@ -1,26 +1,24 @@
 import fs from 'node:fs'
 import { resolve } from 'node:path'
 
-const rename = (src, dest) =>
+export const rename = (src, dest) =>
   fs.renameSync(
     resolve(__dirname, '..', src),
     resolve(__dirname, '..', dest))
 
-const copy = (src, dest) =>
+export const copy = (src, dest) =>
   fs.copyFileSync(
     resolve(__dirname, '..', src),
     resolve(__dirname, '..', dest))
 
-const write = (path, data) =>
+export const write = (path, data) =>
   fs.writeFileSync(resolve(__dirname, '..', path), data)
   
-const read = (path) =>
+export const read = (path) =>
   fs.readFileSync(resolve(__dirname, '..', path), 'utf-8') 
 
-const mkdir = (path) =>
+export const mkdir = (path) =>
   fs.mkdirSync(resolve(__dirname, '..', path))
 
-const readDir = (path) =>
+export const readDir = (path) =>
   fs.readdirSync(resolve(__dirname, '..', path))
-
-module.exports = { rename, copy, mkdir, write, read, readDir }
