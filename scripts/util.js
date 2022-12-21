63ca81cac729a26dcb32519121addfa0ab44e937
Micheal Parks <micheal.parks@outlook.com>
2022-12-21 14:54:14 +0100

    simplify postbuild

--- scripts/util.js
+++ scripts/util.js
@@ -1,27 +1,8 @@
 import fs from 'node:fs'
-import { resolve, dirname } from 'node:path'
-import { fileURLToPath } from 'node:url';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-export const rename = (src, dest) =>
-  fs.renameSync(
-    resolve(__dirname, '..', src),
-    resolve(__dirname, '..', dest))
-
-export const copy = (src, dest) =>
-  fs.copyFileSync(
-    resolve(__dirname, '..', src),
-    resolve(__dirname, '..', dest))
-
-export const write = (path, data) =>
-  fs.writeFileSync(resolve(__dirname, '..', path), data)
-  
-export const read = (path) =>
-  fs.readFileSync(resolve(__dirname, '..', path), 'utf-8') 
-
-export const mkdir = (path) =>
-  fs.mkdirSync(resolve(__dirname, '..', path))
-
-export const readDir = (path) =>
-  fs.readdirSync(resolve(__dirname, '..', path))
+export const rename = (src, dest) => fs.renameSync(src, dest)
+export const copy = (src, dest) => fs.copyFileSync(src, dest)
+export const write = (path, data) => fs.writeFileSync(path, data)
+export const read = (path) => fs.readFileSync(path, 'utf-8') 
+export const mkdir = (path) => fs.mkdirSync(path)
+export const readDir = (path) => fs.readdirSync(path)
