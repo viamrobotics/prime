5f865ae53117febeccc3e2e8802ec22d7af98e1c
Micheal Parks <micheal.parks@outlook.com>
2022-12-21 16:05:09 +0100

    add nojekyll file

--- scripts/util.js
+++ scripts/util.js
@@ -2,7 +2,7 @@ import fs from 'node:fs'
 
 export const rename = (src, dest) => fs.renameSync(src, dest)
 export const copy = (src, dest) => fs.copyFileSync(src, dest)
-export const write = (path, data) => fs.writeFileSync(path, data)
+export const write = (path, data) => fs.writeFileSync(path, data, 'utf-8')
 export const read = (path) => fs.readFileSync(path, 'utf-8') 
 export const mkdir = (path) => fs.mkdirSync(path)
 export const readDir = (path) => fs.readdirSync(path)
