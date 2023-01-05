423d88a98f165544f844cdc6807d56d63c5ba610
micheal-parks <103450731+micheal-parks@users.noreply.github.com>
2023-01-05 18:52:54 +0000

    Deploying to gh-pages from @ viamrobotics/prime@39fde1d040c46a8739f45fd53dd3d8c5194e0b56 🚀

--- assets/icon.stories-e99062bb.js
+++ assets/icon.stories-e99062bb.js
@@ -1,12 +1,13 @@
-import{M as p,C as v,S as m}from"./chunk-MA2MUXQN-bbbbfbe5.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{j as n}from"./jsx-runtime-c27a426b.js";import{u as r}from"./index-0e5a60ed.js";import"./preload-helper-101896b7.js";import"./index-c4c77e71.js";import"./_commonjsHelpers-725317a4.js";import"./index-55ae201a.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-3392a817.js";import"./index-d475d2ea.js";import"./memoizerific-cab73ecf.js";import"./index-014c75af.js";import"./chunk-XHUUYXNA-40ecb194.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-BVZGY62N-610dc239.js";import"./index-10d4cb7f.js";import"./_getTag-25edba2d.js";import"./index-842d733b.js";function u(o={}){const{wrapper:e}=Object.assign({},r(),o.components);return e?n.exports.jsx(e,{...o,children:n.exports.jsx(a,{})}):a();function a(){const s=Object.assign({h1:"h1"},r(),o.components);return n.exports.jsxs(n.exports.Fragment,{children:[n.exports.jsx(p,{title:"Elements/Icon",argTypes:{name:{description:"The icon name",table:{defaultValue:{summary:""}}},size:{description:"The badge status",control:{type:"select"},options:["xs","sm","base","lg","xl","2xl","3xl","4xl"],table:{defaultValue:{summary:"base"}}}}}),`
+import{M as d,C as v,S as m}from"./chunk-MA2MUXQN-bbbbfbe5.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{j as n}from"./jsx-runtime-c27a426b.js";import{u as r}from"./index-0e5a60ed.js";import"./preload-helper-101896b7.js";import"./index-c4c77e71.js";import"./_commonjsHelpers-725317a4.js";import"./index-55ae201a.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-3392a817.js";import"./index-d475d2ea.js";import"./memoizerific-cab73ecf.js";import"./index-014c75af.js";import"./chunk-XHUUYXNA-40ecb194.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-BVZGY62N-610dc239.js";import"./index-10d4cb7f.js";import"./_getTag-25edba2d.js";import"./index-842d733b.js";function u(o={}){const{wrapper:e}=Object.assign({},r(),o.components);return e?n.exports.jsx(e,{...o,children:n.exports.jsx(a,{})}):a();function a(){const s=Object.assign({h1:"h1"},r(),o.components);return n.exports.jsxs(n.exports.Fragment,{children:[n.exports.jsx(d,{title:"Elements/Icon",argTypes:{name:{description:"The icon name",table:{defaultValue:{summary:""}}},size:{description:"The badge status",control:{type:"select"},options:["xs","sm","base","lg","xl","2xl","3xl","4xl"],table:{defaultValue:{summary:"base"}}}}}),`
 `,n.exports.jsx(s.h1,{children:"Icon"}),`
-`,n.exports.jsx(v,{children:n.exports.jsx(m,{name:"Icon",args:{name:"camera",size:"base"},children:({name:l,size:d})=>`
+`,n.exports.jsx(v,{children:n.exports.jsx(m,{name:"Icon",args:{name:"camera",size:"base"},children:({name:l,size:p})=>`
       <v-icon
         name='${l}'
-        size='${d}'
+        size='${p}'
       />
     `})}),`
 `,n.exports.jsx(v,{children:n.exports.jsx(m,{name:"All",children:()=>`
+      <v-icon name='video-camera'></v-icon>
       <v-icon name='error-outline'></v-icon>
       <v-icon name='warning'></v-icon>
       <v-icon name='naught'></v-icon>
@@ -21,11 +22,15 @@ import{M as p,C as v,S as m}from"./chunk-MA2MUXQN-bbbbfbe5.js";import"./chunk-R4
       <v-icon name='download'></v-icon>
       <v-icon name='camera'></v-icon>
       <v-icon name='center'></v-icon>
+      <v-icon name='zoom-out'></v-icon>
       <v-icon name='chevron-down'></v-icon>
       <v-icon name='menu'></v-icon>
+      <v-icon name='more'></v-icon>
       <v-icon name='arrow-up'></v-icon>
       <v-icon name='checkmark'></v-icon>
+      <v-icon name='pop-out'></v-icon>
       <v-icon name='settings'></v-icon>
+      <v-icon name='help-outline'></v-icon>
       <v-icon name='refresh-camera'></v-icon>
       <v-icon name='filter'></v-icon>
       <v-icon name='open-full'></v-icon>
@@ -56,6 +61,7 @@ import{M as p,C as v,S as m}from"./chunk-MA2MUXQN-bbbbfbe5.js";import"./chunk-R4
         size='\${size}'
       />
     \``}};const t=()=>`
+      <v-icon name='video-camera'></v-icon>
       <v-icon name='error-outline'></v-icon>
       <v-icon name='warning'></v-icon>
       <v-icon name='naught'></v-icon>
@@ -70,11 +76,15 @@ import{M as p,C as v,S as m}from"./chunk-MA2MUXQN-bbbbfbe5.js";import"./chunk-R4
       <v-icon name='download'></v-icon>
       <v-icon name='camera'></v-icon>
       <v-icon name='center'></v-icon>
+      <v-icon name='zoom-out'></v-icon>
       <v-icon name='chevron-down'></v-icon>
       <v-icon name='menu'></v-icon>
+      <v-icon name='more'></v-icon>
       <v-icon name='arrow-up'></v-icon>
       <v-icon name='checkmark'></v-icon>
+      <v-icon name='pop-out'></v-icon>
       <v-icon name='settings'></v-icon>
+      <v-icon name='help-outline'></v-icon>
       <v-icon name='refresh-camera'></v-icon>
       <v-icon name='filter'></v-icon>
       <v-icon name='open-full'></v-icon>
@@ -92,6 +102,7 @@ import{M as p,C as v,S as m}from"./chunk-MA2MUXQN-bbbbfbe5.js";import"./chunk-R4
       <v-icon name='linkedin'></v-icon>
       <v-icon name='trash'></v-icon>
     `;t.storyName="All";t.parameters={storySource:{source:`() => \`
+      <v-icon name='video-camera'></v-icon>
       <v-icon name='error-outline'></v-icon>
       <v-icon name='warning'></v-icon>
       <v-icon name='naught'></v-icon>
@@ -106,11 +117,15 @@ import{M as p,C as v,S as m}from"./chunk-MA2MUXQN-bbbbfbe5.js";import"./chunk-R4
       <v-icon name='download'></v-icon>
       <v-icon name='camera'></v-icon>
       <v-icon name='center'></v-icon>
+      <v-icon name='zoom-out'></v-icon>
       <v-icon name='chevron-down'></v-icon>
       <v-icon name='menu'></v-icon>
+      <v-icon name='more'></v-icon>
       <v-icon name='arrow-up'></v-icon>
       <v-icon name='checkmark'></v-icon>
+      <v-icon name='pop-out'></v-icon>
       <v-icon name='settings'></v-icon>
+      <v-icon name='help-outline'></v-icon>
       <v-icon name='refresh-camera'></v-icon>
       <v-icon name='filter'></v-icon>
       <v-icon name='open-full'></v-icon>
@@ -128,4 +143,4 @@ import{M as p,C as v,S as m}from"./chunk-MA2MUXQN-bbbbfbe5.js";import"./chunk-R4
       <v-icon name='linkedin'></v-icon>
       <v-icon name='trash'></v-icon>
     \``}};const i={title:"Elements/Icon",argTypes:{name:{description:"The icon name",table:{defaultValue:{summary:""}}},size:{description:"The badge status",control:{type:"select"},options:["xs","sm","base","lg","xl","2xl","3xl","4xl"],table:{defaultValue:{summary:"base"}}}},tags:["mdx"],includeStories:["icon","all"]};i.parameters=i.parameters||{};i.parameters.docs={...i.parameters.docs||{},page:u};const O=["icon","all"];export{O as __namedExportsOrder,t as all,i as default,c as icon};
-//# sourceMappingURL=icon.stories-d2c7077c.js.map
+//# sourceMappingURL=icon.stories-e99062bb.js.map
