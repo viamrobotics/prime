f5a5737c61d33b607260e0ad81d280b48759b127
Kschappacher <56745262+Kschappacher@users.noreply.github.com>
2023-02-14 10:45:45 -0500

    change indents

--- src/elements/pill.svelte
+++ src/elements/pill.svelte
@@ -1,61 +1,61 @@
 <svelte:options immutable tag="v-pill" />
 
 <script lang="ts">
-  import cx from 'classnames';
+import cx from 'classnames';
 
-  import { htmlToBoolean } from '../lib/boolean';
-  import { dispatcher } from '../lib/dispatch';
-  import { checkKeyboardEvent } from '../lib/events';
-  import { addStyles } from '../lib/index';
+import { htmlToBoolean } from '../lib/boolean';
+import { dispatcher } from '../lib/dispatch';
+import { checkKeyboardEvent } from '../lib/events';
+import { addStyles } from '../lib/index';
 
-  export let value = '';
-  export let removable = 'true';
-  export let readonly: string;
-  export let disabled: string;
+export let value = '';
+export let removable = 'true';
+export let readonly: string;
+export let disabled: string;
 
-  const dispatch = dispatcher();
-  addStyles();
+const dispatch = dispatcher();
+addStyles();
 
-  let isReadonly: boolean;
-  let isDisabled: boolean;
-  let isRemovable: boolean;
+let isReadonly: boolean;
+let isDisabled: boolean;
+let isRemovable: boolean;
 
-  $: isRemovable = htmlToBoolean(removable, 'removable');
-  $: isReadonly = htmlToBoolean(readonly, 'readonly');
-  $: isDisabled = htmlToBoolean(disabled, 'disabled');
+$: isRemovable = htmlToBoolean(removable, 'removable');
+$: isReadonly = htmlToBoolean(readonly, 'readonly');
+$: isDisabled = htmlToBoolean(disabled, 'disabled');
 
-  const handleRemove = (event: MouseEvent | KeyboardEvent) => {
-    if (isDisabled || isReadonly) {
-      return;
-    }
+const handleRemove = (event: MouseEvent | KeyboardEvent) => {
+  if (isDisabled || isReadonly) {
+    return;
+  }
 
-    if (
-      event instanceof KeyboardEvent &&
-      !checkKeyboardEvent(event, ['Enter'])
-    ) {
-      return;
-    }
+  if (
+    event instanceof KeyboardEvent &&
+    !checkKeyboardEvent(event, ['Enter'])
+  ) {
+    return;
+  }
 
-    dispatch('remove', { value });
-  };
+  dispatch('remove', { value });
+};
 </script>
 
 <div
-  class={cx(
-    'flex items-center max-w-fit gap-1 rounded-xl bg-[#C4C4C4] py-0.5 px-2 text-[10px] hover:bg-gray-300',
-    {
-      'bg-black/20 cursor-not-allowed': isDisabled || isReadonly,
-    }
-  )}
-  aria-disabled={isDisabled}
-  aria-readonly={isReadonly}
+class={cx(
+  'flex items-center max-w-fit gap-1 rounded-xl bg-[#C4C4C4] py-0.5 px-2 text-[10px] hover:bg-gray-300',
+  {
+    'bg-black/20 cursor-not-allowed': isDisabled || isReadonly,
+  }
+)}
+aria-disabled={isDisabled}
+aria-readonly={isReadonly}
 >
-  <span>
-    {value}
-  </span>
-  {#if isRemovable}
-    <button on:click={handleRemove} on:keydown={handleRemove}>
-      <v-icon name="x" />
-    </button>
-  {/if}
+<span>
+  {value}
+</span>
+{#if isRemovable}
+  <button on:click={handleRemove} on:keydown={handleRemove}>
+    <v-icon name="x" />
+  </button>
+{/if}
 </div>
