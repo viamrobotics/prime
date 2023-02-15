6ed08f0fd1f7a8f9adc326479c56e95ba7baec0b
Anjali Nainani <anjali.nainani@gmail.com>
2023-02-15 11:04:35 -0500

    APP-1267 - inport theme obj into playground tailwind config

--- playground/tailwind.config.cjs
+++ playground/tailwind.config.cjs
@@ -1,10 +1,9 @@
 const plugin = require('tailwindcss/plugin')
+const { theme } = require('../tailwind.config.cjs')
 
 module.exports = {
   content: ['./index.html', './src/**/*.{ts,vue}', '../src/**/*.{ts,svelte}'],
-  theme: {
-    extend: {},
-  },
+  theme,
   variants: {
     extend: {},
   },
