8787f745c72c7cad59f9b7c2078ca953a05adde6
Kschappacher <56745262+Kschappacher@users.noreply.github.com>
2023-02-21 16:31:46 -0500

    make identifiers more specific

--- playground/src/icon-test.vue
+++ playground/src/icon-test.vue
@@ -4,15 +4,15 @@
 
 <template>
     <main class="m-3">
-        <v-icon data-testid="default-valid" name='camera' />
-        <v-icon data-testid="default-invalid" name='invalidmoose' />
-        <v-icon data-testid="size-xs" name='camera' size='xs' />
-        <v-icon data-testid="size-sm" name='camera' size='sm' />
-        <v-icon data-testid="size-base" name='camera' size='base' />
-        <v-icon data-testid="size-lg" name='camera' size='lg' />
-        <v-icon data-testid="size-xl" name='camera' size='xl' />
-        <v-icon data-testid="size-2xl" name='camera' size='2xl' />
-        <v-icon data-testid="size-3xl" name='camera' size='3xl' />
-        <v-icon data-testid="size-4xl" name='camera' size='4xl' />
+        <v-icon data-testid="icon-default-valid" name='camera' />
+        <v-icon data-testid="icon-default-invalid" name='invalidmoose' />
+        <v-icon data-testid="icon-size-xs" name='camera' size='xs' />
+        <v-icon data-testid="icon-size-sm" name='camera' size='sm' />
+        <v-icon data-testid="icon-size-base" name='camera' size='base' />
+        <v-icon data-testid="icon-size-lg" name='camera' size='lg' />
+        <v-icon data-testid="icon-size-xl" name='camera' size='xl' />
+        <v-icon data-testid="icon-size-2xl" name='camera' size='2xl' />
+        <v-icon data-testid="icon-size-3xl" name='camera' size='3xl' />
+        <v-icon data-testid="icon-size-4xl" name='camera' size='4xl' />
     </main>
 </template>
--- tests/Icon.spec.ts
+++ tests/Icon.spec.ts
@@ -4,31 +4,31 @@ test('Icons render as expected', async ({ page }) => {
     await page.goto('/test.html');
 
     // default valid
-    await expect(page.getByTestId("default-valid")).toBeVisible()
+    await expect(page.getByTestId("icon-default-valid")).toBeVisible()
     // default invalid
-    await expect(page.getByTestId("default-invalid")).not.toBeVisible()
+    await expect(page.getByTestId("icon-default-invalid")).not.toBeVisible()
     // size-xs
-    await expect(page.getByTestId("size-xs")).toBeVisible()
-    await expect(page.getByTestId("size-xs").locator('i')).toHaveClass(/text-xs/)
+    await expect(page.getByTestId("icon-size-xs")).toBeVisible()
+    await expect(page.getByTestId("icon-size-xs").locator('i')).toHaveClass(/text-xs/)
     // size-sm
-    await expect(page.getByTestId("size-sm")).toBeVisible()
-    await expect(page.getByTestId("size-sm").locator('i')).toHaveClass(/text-sm/)
+    await expect(page.getByTestId("icon-size-sm")).toBeVisible()
+    await expect(page.getByTestId("icon-size-sm").locator('i')).toHaveClass(/text-sm/)
     // size-base
-    await expect(page.getByTestId("size-base")).toBeVisible()
-    await expect(page.getByTestId("size-base").locator('i')).toHaveClass(/text-base/)
+    await expect(page.getByTestId("icon-size-base")).toBeVisible()
+    await expect(page.getByTestId("icon-size-base").locator('i')).toHaveClass(/text-base/)
     // size-lg
-    await expect(page.getByTestId("size-lg")).toBeVisible()
-    await expect(page.getByTestId("size-lg").locator('i')).toHaveClass(/text-lg/)
+    await expect(page.getByTestId("icon-size-lg")).toBeVisible()
+    await expect(page.getByTestId("icon-size-lg").locator('i')).toHaveClass(/text-lg/)
     // size-xl
-    await expect(page.getByTestId("size-xl")).toBeVisible()
-    await expect(page.getByTestId("size-xl").locator('i')).toHaveClass(/text-xl/)
+    await expect(page.getByTestId("icon-size-xl")).toBeVisible()
+    await expect(page.getByTestId("icon-size-xl").locator('i')).toHaveClass(/text-xl/)
     // size-2xl
-    await expect(page.getByTestId("size-2xl")).toBeVisible()
-    await expect(page.getByTestId("size-2xl").locator('i')).toHaveClass(/text-2xl/)
+    await expect(page.getByTestId("icon-size-2xl")).toBeVisible()
+    await expect(page.getByTestId("icon-size-2xl").locator('i')).toHaveClass(/text-2xl/)
     // size-3xl
-    await expect(page.getByTestId("size-3xl")).toBeVisible()
-    await expect(page.getByTestId("size-3xl").locator('i')).toHaveClass(/text-3xl/)
+    await expect(page.getByTestId("icon-size-3xl")).toBeVisible()
+    await expect(page.getByTestId("icon-size-3xl").locator('i')).toHaveClass(/text-3xl/)
     // size-4xl
-    await expect(page.getByTestId("size-4xl")).toBeVisible()
-    await expect(page.getByTestId("size-4xl").locator('i')).toHaveClass(/text-4xl/)
+    await expect(page.getByTestId("icon-size-4xl")).toBeVisible()
+    await expect(page.getByTestId("icon-size-4xl").locator('i')).toHaveClass(/text-4xl/)
 });
