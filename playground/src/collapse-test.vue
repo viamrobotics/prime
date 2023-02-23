2cb7e000e37e8ae118296515417d47fe7a6fc1a6
emily hong <emily.hong@viam.com>
2023-02-23 14:23:16 -0500

    wrote tests, updated collapse to do string to boolean conversion

--- playground/src/collapse-test.vue
+++ playground/src/collapse-test.vue
@@ -8,8 +8,21 @@
       data-testid="collapse"
       title='Robots...'
     >
-      <div slot='title'>...(title)...</div>
+      <div slot='title'>(title)</div>
+      <div slot='header'>(header)</div>
       <div>...are pretty cool!</div>
+    </v-collapse>
+
+    <v-collapse
+      data-testid="collapse-open"
+      open="true"
+    >
+    </v-collapse>
+
+    <v-collapse
+      data-testid="collapse-closed"
+      open="false"
+    >
     </v-collapse>
   </main>
 </template>
--- tests/collapse.spec.ts
+++ tests/collapse.spec.ts
@@ -3,55 +3,60 @@ import { test, expect } from '@playwright/test';
 test('Collapse E2E Tests', async ({ page }) => {
   await page.goto('/test.html');
 
-  // Title
-  const collapseTitle = page.getByText('Robots...')
-
-  await expect(collapseTitle).toBeVisible()
-  await expect(collapseTitle).toHaveText('Robots...')
-
-  // Slot: title
-  const collapseTitleSlot = page.getByText('...(title)...')
-  await expect(collapseTitleSlot).toBeVisible()
-  await expect(collapseTitleSlot).toHaveText('...(title)...')
-
-  // Header: open
-  // expect(collapseTitle.getAttribute("title")).toBeTruthy
-
-  // Header: open
-
-  // GIVEN a "open" attribute has been applied to the v-collapse element
-  // AND the "open" attribute value is true WHEN the element is rendered
-  // THEN the collapse should render the header with the  chevron pointing down
-
-  // Header: closed
-
-  // GIVEN a "open" attribute has not been applied to the  v-collapse element
-  // OR a "open" attribute has been applied to the v-collapse element
-  // AND the "open" attribute value is falseWHEN the element is rendered
-  // THEN the collapse should render the header with the chevron pointing down
   const collapse = page.getByTestId('collapse')
+
+  // Main title should display
+  const collapseTitleText = 'Robots...'
+  const collapseTitle = collapse.getByText(collapseTitleText)
+  await expect(collapseTitle).toBeVisible()
+  await expect(collapseTitle).toHaveText(collapseTitleText)
+
+  // Slot:title should display
+  const titleSlotText = '(title)'
+  const titleSlot = collapse.getByText(titleSlotText)
+  await expect(titleSlot).toBeVisible()
+  await expect(titleSlot).toHaveText(titleSlotText)
+
+  // Open collapse (by "open" property) should display chevron up
+  const openCollapse = page.getByTestId('collapse-open')
+  await expect(openCollapse).toHaveJSProperty('open','true')
+  await expect(openCollapse.locator('i')).toHaveClass(/icon-chevron-down/)
+  await expect(openCollapse.locator('i')).toBeVisible()
+  await expect(openCollapse.locator('v-icon')).toHaveClass(/rotate-180/)
+
+  // Closed collapse should display chevron down
+  // no "open" property, default display closed
   await expect(collapse.locator('i')).toHaveClass(/icon-chevron-down/)
   await expect(collapse.locator('i')).toBeVisible()
   await expect(collapse.locator('v-icon')).toHaveClass(/rotate-0/)
+  // "open" property "false" has been applied
+  const closedCollapse = page.getByTestId('collapse-closed')
+  await expect(closedCollapse.locator('i')).toHaveClass(/icon-chevron-down/)
+  await expect(closedCollapse.locator('i')).toBeVisible()
+  await expect(closedCollapse.locator('v-icon')).toHaveClass(/rotate-0/)
 
-  // await expect(chevronDown.getAttribute('class')).toBe(/rotate-0/)
+  // Slot:header should display
+  const headerSlotText = '(header)'
+  const headerSlot = page.getByText(headerSlotText)
+  await expect(headerSlot).toBeVisible()
+  await expect(headerSlot).toHaveText(headerSlotText)
+  // THEN the collapse should render the child element  before the chevron in the collapse header
 
-  // await expect(chevronDown).toHaveClass(/rotate-0/)
+  // If closed, after click, collapse should open (and display child)
+  await expect(collapse).toHaveJSProperty('open', 'false')
+  await collapse.click()
+  const expandText = '...are pretty cool!'
+  const expand = collapse.getByText(expandText)
+  await expect(expand).toBeVisible()
+  await expect(expand).toHaveText(expandText)
+  await expect(openCollapse.locator('i')).toHaveClass(/icon-chevron-down/)
+  await expect(openCollapse.locator('i')).toBeVisible()
+  await expect(openCollapse.locator('v-icon')).toHaveClass(/rotate-180/)
 
-  // Slot: header
-
-  // GIVEN a child element is included in the v-collapseAND the child element has a "slot"AND the "slot" attribute value is "header"WHEN the element is renderedTHEN the collapse should render the child element  before the chevron in the collapse header
-
-  // Click: open
-
-  // GIVEN the v-collapse is closedWHEN the v-collapse header is clickedTHEN the collapse should open
-
-  // Click: close
-
-  // GIVEN the v-collapse is openWHEN the v-collapse header is clickedTHEN the collapse should close
-
-  // Children
-
-  // GIVEN a child element is included in the v-collapseAND the child element does not have a "slot" attributeOR the "slot" attribute value is not "title"OR the "slot" attribute value is not "header"WHEN the element is renderedAND the collapse is openTHEN the collapse should render the child element  under the collapse header
-
+  // If open, after click, collapse should close (and hide child)
+  await collapse.click()
+  await expect(expand).toBeHidden()
+  await expect(closedCollapse.locator('i')).toHaveClass(/icon-chevron-down/)
+  await expect(closedCollapse.locator('i')).toBeVisible()
+  await expect(closedCollapse.locator('v-icon')).toHaveClass(/rotate-0/)
 });
