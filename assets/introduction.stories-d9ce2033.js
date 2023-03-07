77cbb95ce1eb7f57b1da822892bec91ad0c6e5cd
ehhong <31592018+ehhong@users.noreply.github.com>
2023-03-07 19:10:58 +0000

    Deploying to gh-pages from @ viamrobotics/prime@a6b1eb4c40200ebf869bfbb232f136e5f74fbff6 🚀

--- assets/introduction.stories-d9ce2033.js
+++ assets/introduction.stories-d9ce2033.js
@@ -1,4 +1,4 @@
-import{M as p}from"./chunk-MA2MUXQN-bbbbfbe5.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{j as e}from"./jsx-runtime-c27a426b.js";import{u as i}from"./index-0e5a60ed.js";import"./preload-helper-101896b7.js";import"./index-c4c77e71.js";import"./_commonjsHelpers-725317a4.js";import"./index-55ae201a.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-3392a817.js";import"./index-d475d2ea.js";import"./memoizerific-cab73ecf.js";import"./index-014c75af.js";import"./chunk-XHUUYXNA-40ecb194.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-BVZGY62N-610dc239.js";import"./index-10d4cb7f.js";import"./_getTag-25edba2d.js";import"./index-842d733b.js";function a(r={}){const{wrapper:t}=Object.assign({},i(),r.components);return t?e.exports.jsx(t,Object.assign({},r,{children:e.exports.jsx(o,{})})):o();function o(){const s=Object.assign({h1:"h1",p:"p",h2:"h2",ul:"ul",li:"li",code:"code",pre:"pre",h3:"h3",img:"img",a:"a"},i(),r.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(s.h1,{children:"P.R.I.M.E."}),`
+import{M as p}from"./chunk-MA2MUXQN-bbbbfbe5.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{j as e}from"./jsx-runtime-c27a426b.js";import{u as i}from"./index-0e5a60ed.js";import"./preload-helper-101896b7.js";import"./index-c4c77e71.js";import"./_commonjsHelpers-725317a4.js";import"./index-55ae201a.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-3392a817.js";import"./index-d475d2ea.js";import"./memoizerific-cab73ecf.js";import"./index-014c75af.js";import"./chunk-XHUUYXNA-40ecb194.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-BVZGY62N-610dc239.js";import"./index-10d4cb7f.js";import"./_getTag-25edba2d.js";import"./index-842d733b.js";function a(r={}){const{wrapper:t}=Object.assign({},i(),r.components);return t?e.exports.jsx(t,Object.assign({},r,{children:e.exports.jsx(n,{})})):n();function n(){const s=Object.assign({h1:"h1",p:"p",h2:"h2",ul:"ul",li:"li",code:"code",pre:"pre",h3:"h3",img:"img",a:"a"},i(),r.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(s.h1,{children:"P.R.I.M.E."}),`
 `,e.exports.jsx(s.p,{children:"Pretty Rad Interactive Modular Elements."}),`
 `,e.exports.jsx(s.p,{children:"Made with ❤️ at Viam."}),`
 `,e.exports.jsx(s.h2,{children:"Getting started"}),`
@@ -44,6 +44,9 @@ npm run check
 `,e.exports.jsx(s.pre,{children:e.exports.jsx(s.code,{className:"language-bash",children:`npm run test-dev # to test with the playwright debug UI
 # or
 npm run test # to test in headless mode
+
+# to run specific tests
+npm run test <test-name> # for <test-name>.spec.ts
 `})}),`
 `,e.exports.jsx(s.h3,{children:"Browser Compatibility"}),`
 `,e.exports.jsxs(s.p,{children:[e.exports.jsx(s.code,{children:"PRIME"})," supports the following browsers:"]}),`
@@ -52,6 +55,6 @@ npm run test # to test in headless mode
 Latest ✔ | Latest ✔ | Latest ✔ | Latest ✔ |`]}),`
 `,e.exports.jsx(s.h2,{children:"License"}),`
 `,e.exports.jsx(s.p,{children:"Copyright 2021-2022 Viam Inc."}),`
-`,e.exports.jsxs(s.p,{children:["Apache 2.0 - See ",e.exports.jsx(s.a,{href:"https://github.com/viamrobotics/prime/blob/main/LICENSE",children:"LICENSE"})," file"]})]})}}function c(r={}){const{wrapper:t}=Object.assign({},i(),r.components);return t?e.exports.jsx(t,{...r,children:e.exports.jsx(o,{})}):o();function o(){return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(p,{title:"Docs/Introduction"}),`
-`,e.exports.jsx(a,{})]})}}const l=()=>{throw new Error("Docs-only story")};l.parameters={docsOnly:!0};const n={title:"Docs/Introduction",tags:["mdx"],includeStories:["__page"]};n.parameters=n.parameters||{};n.parameters.docs={...n.parameters.docs||{},page:c};const S=["__page"];export{S as __namedExportsOrder,l as __page,n as default};
-//# sourceMappingURL=introduction.stories-b90e851b.js.map
+`,e.exports.jsxs(s.p,{children:["Apache 2.0 - See ",e.exports.jsx(s.a,{href:"https://github.com/viamrobotics/prime/blob/main/LICENSE",children:"LICENSE"})," file"]})]})}}function c(r={}){const{wrapper:t}=Object.assign({},i(),r.components);return t?e.exports.jsx(t,{...r,children:e.exports.jsx(n,{})}):n();function n(){return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(p,{title:"Docs/Introduction"}),`
+`,e.exports.jsx(a,{})]})}}const l=()=>{throw new Error("Docs-only story")};l.parameters={docsOnly:!0};const o={title:"Docs/Introduction",tags:["mdx"],includeStories:["__page"]};o.parameters=o.parameters||{};o.parameters.docs={...o.parameters.docs||{},page:c};const S=["__page"];export{S as __namedExportsOrder,l as __page,o as default};
+//# sourceMappingURL=introduction.stories-d9ce2033.js.map
