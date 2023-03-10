6e88dbd5937ad822d11b2f410fb80f494afea2fd
Anjali Nainani <anjali.nainani@gmail.com>
2023-03-10 16:45:19 -0500

    APP-1386 - progress

--- tests/tooltip.spec.ts
+++ tests/tooltip.spec.ts
@@ -32,6 +32,13 @@ test('Renders tooltip text below upon hover if location attribute is specified a
   await expect(tooltipTextTop).toHaveText('This is the bottom test')
 });
 
+test('Renders tooltip text  upon hover if location attribute is specified as "bottom"', async ({ page }) => {
+  await page.goto('/test.html');
+
+  const tooltipTop = page.getByText('Bottom Test')
+  const tooltipTextTop = page.locator("div.absolute top-0 left-0 bg-gray-9 text-white text-left text-xs py-1 px-2 border border-gray-9 z-[1000]:below(:text('Top Test'))").first()
+  await expect(tooltipTextTop).toHaveText('This is the bottom test')
+});
 
 
   // // Tooltip Bottom Test
