0d28a26f67875b5ab25646af741140216cfb88b6
Anjali Nainani <anjali.nainani@gmail.com>
2023-03-10 13:12:05 -0500

    APP-1386 - rewrite hover

--- tests/tooltip.spec.ts
+++ tests/tooltip.spec.ts
@@ -1,23 +1,36 @@
 import { test, expect } from '@playwright/test';
 import config from '../tailwind.config.cjs'
 
-test('Tooltip E2E Tests', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
+  await page.goto('/test.html');
+});
+
+test('Confirms tooltip text is visible upon hover', async ({ page }) => {
   await page.goto('/test.html');
 
   // Tooltip Hover Test
-  const tooltipHover = page.getByText('Hover here - Hover Test')
+  const tooltipHover = page.getByText('Hover Test')
   const tooltipText = page.getByRole('tooltip', { name: 'This is the hover test'})
   await tooltipHover.hover()
   await expect(tooltipText).toBeVisible()
   await expect(tooltipText).toHaveText('This is the hover test')
+});
 
-  // Tooltip Top Test
-  const tooltipLeft = page.locator("div.icon-info-outline:above(:text('Tool 10'))").first()
-  await expect(tooltipLeft).toHaveClass(/icon-info-outline/)
+test('Renders tooltip text above upon hover if location attribute is specified as "top"', async ({ page }) => {
+  await page.goto('/test.html');
 
-  // Tooltip Bottom Test
-  const tooltipRight = page.locator("div.icon-info-outline:below(:text('Tool 10'))").first()
-  await expect(tooltipRight).toHaveClass()
+  const tooltipTop = page.getByText('Top Test')
+  const tooltipTextTop = page.locator("div.absolute top-0 left-0 bg-gray-9 text-white text-left text-xs py-1 px-2 border border-gray-9 z-[1000]:above(:text('Top Test'))").first().textContent()
+  await expect(tooltipTextTop).toContain('This is the top test')
+});
+
+  // // Tooltip Top Test
+  // const tooltipLeft = page.locator("div.icon-info-outline:above(:text('Tool 10'))").first()
+  // await expect(tooltipLeft).toHaveClass(/icon-info-outline/)
+
+  // // Tooltip Bottom Test
+  // const tooltipRight = page.locator("div.icon-info-outline:below(:text('Tool 10'))").first()
+  // await expect(tooltipRight).toHaveClass()
 
   // Tooltip Left Test
 
@@ -26,4 +39,3 @@ test('Tooltip E2E Tests', async ({ page }) => {
   // Tooltip Default Test (Top)
 
 
-});
