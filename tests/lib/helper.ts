68f56c770d477e2f258b0cf0d97b0a2624b3e762
emily hong <31592018+ehhong@users.noreply.github.com>
2023-03-14 12:23:12 -0400

    APP-1387 Add helper function for testing dispatched custom events with parameters

--- tests/lib/helper.ts
+++ tests/lib/helper.ts
@@ -1,15 +1,62 @@
 import { expect,Page } from '@playwright/test';
+import config from '../../tailwind.config.cjs'
 
-export const waitForCustomEvent = async(page:Page, customEventName:string) => {
-    return page.evaluate(eventName => new Promise(callback => window.addEventListener(eventName, callback,{ once: true })), customEventName)
+export const hexToRGB = (color?: string) => {
+  const hex = config.theme.extend.colors[color];
+
+  if (!hex) {
+    throw new Error(`hex not found for color: ${color}`)
+  }
+
+  let alpha = false;
+  let h = hex.slice(hex.startsWith('#') ? 1 : 0);
+  if (h.length === 3) h = [...h].map(x => x + x).join('');
+  else if (h.length === 8) alpha = true;
+  h = Number.parseInt(h, 16);
+  return (
+    `rgb${ 
+      alpha ? 'a' : '' 
+    }(${ 
+      h >>> (alpha ? 24 : 16) 
+    }, ${ 
+      (h & (alpha ? 0x00_FF_00_00 : 0x00_FF_00)) >>> (alpha ? 16 : 8) 
+    }, ${ 
+      (h & (alpha ? 0x00_00_FF_00 : 0x00_00_FF)) >>> (alpha ? 8 : 0) 
+    }${alpha ? `, ${h & 0x00_00_00_FF}` : '' 
+    })`
+  );
+};
+
+export const waitForCustomEvent = async (page:Page, customEventName:string) => {
+  return page.evaluate(eventName => new Promise(callback => window.addEventListener(eventName, callback,{ once: true })), customEventName)
 }
 
 export const waitForCustomEventTimeout = async (page:Page,customEventName:string) => {
-    const value = await Promise.race([
-        page.evaluate(eventName => new Promise(callback => window.addEventListener(eventName, callback, { once: true })), customEventName),
-        new Promise((resolve) => {setTimeout(resolve, 500, "timeout")}),
-    ])
+  const value = await Promise.race([
+    page.evaluate(eventName => new Promise(callback => window.addEventListener(eventName, callback, { once: true })), customEventName),
+    new Promise((resolve) => {setTimeout(resolve, 500, "timeout")}),
+  ])
 
-    expect(value).toBe("timeout")
+  expect(value).toBe("timeout")
 }
 
+// This function adds an event listener for eventName and puts the value of paramName on the window
+export const waitForCustomEventWithParam = async (page:Page, eventName:string, paramName:string) => {
+  return page.evaluate(
+    (eventInfo) => {
+      function listener(event) {
+        const dispatchedEvent = { [eventInfo.eventName]: { [eventInfo.paramName]: event.target[eventInfo.paramName] } }
+        window['__testingCustomEvents'] = dispatchedEvent
+      }
+      return new Promise(resolve => {
+        window.addEventListener(eventInfo.eventName, listener, { once: true })
+        resolve(eventInfo.eventName + ' event called!')
+      })}, 
+  {eventName, paramName})
+}
+
+// This function retrieves the dispatched event's parameter value, as set by waitForCustomEventWithParam()
+// Call this only after setting up an event listener with waitForCustomEventWithParam()
+export const getCustomEventParam = async (page:Page, eventName:string, paramName:string) => {
+    return page.evaluate(`window.__testingCustomEvents.${eventName}.${paramName}`)
+}
