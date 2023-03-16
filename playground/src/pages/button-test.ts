96634c74e95f638e6e42d788e531c2656ae94f35
Anjali Nainani <anjali.nainani@gmail.com>
2023-03-16 12:42:42 -0400

    APP-1418 - button e2e tests

--- playground/src/pages/button-test.ts
+++ playground/src/pages/button-test.ts
@@ -1,6 +1,6 @@
 import './main.css'
-import '../../src/main'
+import '../../../src/main'
 import { createApp } from 'vue'
-import App from './button-test.vue'
+import App from '../button-test.vue'
 
 createApp(App).mount('#app')
--- tests/button.spec.ts
+++ tests/button.spec.ts
@@ -1,107 +1,93 @@
 import { test, expect } from '@playwright/test';
-import { hexToRGB, waitForCustomEventTimeout, waitForCustomEventWithParam, getCustomEventParam } from './lib/helper.ts'
+import { waitForCustomEventWithParam, getCustomEventParam } from './lib/helper.ts'
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/button-test.html');
 });
 
 test('Renders label attribute as text within the button', async ({ page }) => {
+  const labelButton = page.getByTestId('label')
+  await expect(labelButton.locator('button')).toBeVisible()
+  await expect(labelButton.locator('button')).toContainText('Label')
 });
 
 test('Renders a button in the style of the primary variant if no variant is specified', async ({ page }) => {
+  const defaultButton = page.getByTestId('default-primary')
+  await expect(defaultButton.locator('button')).toBeVisible()
+  await expect(defaultButton.locator('button')).toHaveClass(/bg-white/)
+  await expect(defaultButton.locator('button')).toHaveClass(/border-gray-9/)
 })
 
 test('Renders a button in the style of the primary variant if variant is specified as primary', async ({ page }) => {
+  const primaryButton = page.getByTestId('primary')
+  await expect(primaryButton.locator('button')).toBeVisible()
+  await expect(primaryButton.locator('button')).toHaveClass(/bg-white/)
+  await expect(primaryButton.locator('button')).toHaveClass(/border-gray-9/)
+
 })
 
 test('Renders a button in the style of inverse primary if the variant is specified as inverse primary', async ({ page }) => {
+  const inversePrimaryButton = page.getByTestId('inverse-primary')
+  await expect(inversePrimaryButton.locator('button')).toBeVisible()
+  await expect(inversePrimaryButton.locator('button')).toHaveClass(/bg-gray-9/)
+  await expect(inversePrimaryButton.locator('button')).toHaveClass(/border-gray-9/)
+  await expect(inversePrimaryButton.locator('button')).toHaveClass(/text-white/)
 })
 
 test('Renders a button in the style of success if the variant is specified as success', async ({ page }) => {
+  const successButton = page.getByTestId('success')
+  await expect(successButton.locator('button')).toBeVisible()
+  await expect(successButton.locator('button')).toHaveClass(/bg-success-fg/)
+  await expect(successButton.locator('button')).toHaveClass(/border-success-fg/)
+  await expect(successButton.locator('button')).toHaveClass(/text-white/)
 })
 
 test('Renders a button in the style of danger if the variant is specified as danger', async ({ page }) => {
+  const dangerButton = page.getByTestId('danger')
+  await expect(dangerButton.locator('button').first()).toBeVisible()
+  await expect(dangerButton.locator('button').first()).toHaveClass(/bg-danger-fg/)
+  await expect(dangerButton.locator('button').first()).toHaveClass(/border-danger-fg/)
+  await expect(dangerButton.locator('button').first()).toHaveClass(/text-white/)
 })
 
 test('Renders a button in the style of outline danger if the variant is specified as outline danger', async ({ page }) => {
+  const outlineDangerButton = page.getByTestId('outline-danger')
+  await expect(outlineDangerButton.locator('button')).toBeVisible()
+  await expect(outlineDangerButton.locator('button')).toHaveClass(/bg-danger-bg/)
+  await expect(outlineDangerButton.locator('button')).toHaveClass(/border-danger-bg/)
+  await expect(outlineDangerButton.locator('button')).toHaveClass(/text-danger-fg/)
 })
 
-test('Renders a button as disabled if ', async ({ page }) => {
+test('Renders a button as disabled if the button has a disabled attribute of true', async ({ page }) => {
+  const disabledButton = page.getByRole('button', { name: 'Disabled Label' })
+  await expect(disabledButton).toBeVisible()
+  await expect(disabledButton).toBeDisabled()
 })
 
-test('Renders as disabled', async ({ page }) => {
-  const switchDisabledOff = page.getByTestId('switch-disabled-off')
-  await expect(switchDisabledOff).toBeVisible()
-  await expect(switchDisabledOff.getByRole('switch')).toBeDisabled()
-  await expect(switchDisabledOff.locator('input')).toBeDisabled()
-  await expect(switchDisabledOff.getByText('disabled')).toHaveCSS('color', hexToRGB('text-disabled'))
-  await expect(switchDisabledOff.getByText('off')).toHaveCSS('color', hexToRGB('text-disabled'))
-  await expect(switchDisabledOff.getByRole('switch').locator('div').first()).toHaveCSS("background-color", hexToRGB('gray-4'))
-
-  const switchDisabledOn = page.getByTestId('switch-disabled-on')
-  await expect(switchDisabledOn).toBeVisible()
-  await expect(switchDisabledOn.locator('input')).toHaveValue('on')
-  await expect(switchDisabledOn.getByRole('switch')).toBeDisabled()
-  await expect(switchDisabledOn.locator('input')).toBeDisabled()
-  await expect(switchDisabledOn.getByText('disabled')).toHaveCSS('color', hexToRGB('text-disabled'))
-  await expect(switchDisabledOn.getByText('on')).toHaveCSS('color', hexToRGB('text-disabled'))
-  await expect(switchDisabledOn.getByRole('switch').locator('div').first()).toHaveCSS("background-color", hexToRGB('gray-4'))
+test('Renders a tooltip over the button if a tooltip attribute is specified', async ({ page }) => {
+  const tooltipButton = page.getByRole('button', { name: 'Tooltip Label' })
+  const tooltipText = page.getByRole('tooltip', { name: 'This is the message for the tooltip'})
+  await expect(tooltipButton).toBeVisible()
+  await tooltipButton.hover()
+  await expect(tooltipText).toBeVisible()
+  await expect(tooltipText).toHaveText('This is the message for the tooltip')
 })
 
-test('Precludes value change if disabled', async ({ page }) => {
-  const switchDisabledOff = page.getByTestId('switch-disabled-off')
-  const isInputEventEmitted = waitForCustomEventTimeout(page,'input')
-  await switchDisabledOff.locator('label').click({ force: true })
-  await expect(switchDisabledOff.locator('input')).toHaveValue('off')
-  await isInputEventEmitted
+test('Renders an icon within the button/next to the label if an icon attribute is specified', async ({ page }) => {
+  const iconWithinButton = page.getByRole('button', { name: 'Icon Within Button Label' })
+  await expect(iconWithinButton).toBeVisible()
+  await expect(iconWithinButton).toContainText('Label')
+  await expect(iconWithinButton.locator('i')).toHaveClass(/icon-refresh/)
 })
 
-
-test('Renders as read only', async ({ page }) => {
-  const switchReadOnlyOff = page.getByTestId('switch-readonly-off')
-  await expect(switchReadOnlyOff).toBeVisible()
-  await expect(switchReadOnlyOff.locator('input')).not.toBeEditable()
-  await expect(switchReadOnlyOff.getByText('read only')).not.toHaveCSS('color', hexToRGB('text-disabled'))
-  await expect(switchReadOnlyOff.getByText('off')).not.toHaveCSS('color', hexToRGB('text-disabled'))
-  await expect(switchReadOnlyOff.getByRole('switch').locator('div').first()).toHaveCSS("background-color", hexToRGB('gray-4'))
-
-  const switchReadOnlyOn = page.getByTestId('switch-readonly-on')
-  await expect(switchReadOnlyOn).toBeVisible()
-  await expect(switchReadOnlyOn.locator('input')).toHaveValue('on')
-  await expect(switchReadOnlyOn.locator('input')).not.toBeEditable()
-  await expect(switchReadOnlyOn.getByText('read only')).not.toHaveCSS('color', hexToRGB('text-disabled'))
-  await expect(switchReadOnlyOn.getByText('on', { exact: true })).not.toHaveCSS('color', hexToRGB('text-disabled'))
-  await expect(switchReadOnlyOn.getByRole('switch').locator('div').first()).toHaveCSS("background-color", hexToRGB('gray-4'))
+test('Renders the button as an icon if an icon attribute is specified and the button variant is icon', async ({ page }) => {
+  const iconAsButton = page.getByRole('button', { name: 'Icon As Button' })
+  await expect(iconAsButton).toBeVisible()
+  await expect(iconAsButton).not.toContainText('Icon As Button')
+  await expect(iconAsButton.locator('i')).toHaveClass(/icon-refresh/)
 })
 
-test('Precludes value change if read only', async ({ page }) => {
-  const switchReadOnlyOff = page.getByTestId('switch-readonly-off')
-  const isInputEventEmitted = waitForCustomEventTimeout(page,'input')
-  await switchReadOnlyOff.locator('label').click({ force: true })
-  await expect(switchReadOnlyOff.locator('input')).toHaveValue('off')
-  await isInputEventEmitted
-})
-
-test('Displays tooltip in correct position given tooltip attribute', async ({ page }) => {
-  const switchTooltipDefault = page.getByTestId('switch-default')
-  await expect(switchTooltipDefault).toBeVisible()
-  await expect(switchTooltipDefault.locator('v-tooltip')).toBeVisible()
-  await expect(switchTooltipDefault.locator('v-tooltip:above(button)').first()).toBeVisible()
-  await expect(switchTooltipDefault.locator('v-tooltip:right-of(:text("Default"))').first()).toBeVisible()
-
-  const switchTooltipLeft = page.getByTestId('switch-label-left')
-  await expect(switchTooltipLeft).toBeVisible()
-  await expect(switchTooltipLeft.locator('v-tooltip')).toBeVisible()
-  await expect(switchTooltipLeft.locator('v-tooltip:left-of(button)').first()).toBeVisible()
-  await expect(switchTooltipLeft.locator('v-tooltip:right-of(:text("Left"))').first()).toBeVisible()
-})
-
-test('If variant is annotated, displays annotations on right side of switch', async ({ page }) => {
-  const switchAnnotated = page.getByTestId('switch-annotated')
-  await expect(switchAnnotated).toBeVisible()
-  await expect(switchAnnotated.getByText('Off')).toBeVisible()
-  await expect(switchAnnotated.locator('span:left-of(:text("Off"))').first()).toBeVisible()
-  await switchAnnotated.locator('label').click()
-  await expect(switchAnnotated.getByText('On')).toBeVisible()
-  await expect(switchAnnotated.locator('span:left-of(:text("On"))').first()).toBeVisible()
+test('Tests that the button is clickable if not disabled', async ({ page }) => {
+  const clickMeButton = page.getByRole('button', { name: 'Click Me' })
+  await clickMeButton.click()
 })
