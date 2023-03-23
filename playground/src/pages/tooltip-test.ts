f21b61e222f139c6a6d0e496629de32e2a26d860
Anjali Nainani <anjali.nainani@gmail.com>
2023-03-23 11:15:13 -0400

    APP-1386 - format

--- playground/src/pages/tooltip-test.ts
+++ playground/src/pages/tooltip-test.ts
@@ -1,6 +1,6 @@
-import './main.css'
-import '../../../src/main'
-import { createApp } from 'vue'
-import App from '../tooltip-test.vue'
+import './main.css';
+import '../../../src/main';
+import { createApp } from 'vue';
+import App from '../tooltip-test.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
