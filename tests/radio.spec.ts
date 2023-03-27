a4203ef0ee667344baf0f7ba43356d71df136d6d
Micheal Parks <micheal.parks@outlook.com>
2023-03-27 13:14:42 -0400

    fix tests

--- tests/radio.spec.ts
+++ tests/radio.spec.ts
@@ -134,7 +134,7 @@ test('Radio tooltips are rendered as warn icon when state attribute is set to wa
     .getByTestId('radio-tooltip-warn-test')
     .locator('v-tooltip div')
     .first();
-  await expect(tooltipWarn).toHaveClass(/icon-error-outline text-warning-fg/);
+  await expect(tooltipWarn).toHaveClass(/icon-error-outline text-warning-bright/);
 });
 
 test('Radio tooltips are rendered as warn icon when state attribute is set to error', async ({
