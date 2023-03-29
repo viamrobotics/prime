6304750f2f6f276956f44f6a77f2589ca7c5272c
mcous <michael@cousins.io>
2023-03-29 13:57:44 +0000

    Deploying to gh-pages from @ viamrobotics/prime@de35a8458340f3c8e2b3fa327d2cf8385cf000ff 🚀

--- assets/contributing.stories-9fdba5a6.js
+++ assets/contributing.stories-9fdba5a6.js
@@ -1,4 +1,4 @@
-import{M as p}from"./chunk-MA2MUXQN-bbbbfbe5.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{j as e}from"./jsx-runtime-c27a426b.js";import{u as i}from"./index-0e5a60ed.js";import"./preload-helper-101896b7.js";import"./index-c4c77e71.js";import"./_commonjsHelpers-725317a4.js";import"./index-55ae201a.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-3392a817.js";import"./index-d475d2ea.js";import"./memoizerific-cab73ecf.js";import"./index-014c75af.js";import"./chunk-XHUUYXNA-40ecb194.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-BVZGY62N-610dc239.js";import"./index-10d4cb7f.js";import"./_getTag-25edba2d.js";import"./index-842d733b.js";function c(n={}){const{wrapper:s}=Object.assign({},i(),n.components);return s?e.exports.jsx(s,Object.assign({},n,{children:e.exports.jsx(r,{})})):r();function r(){const t=Object.assign({h1:"h1",p:"p",code:"code",h2:"h2",pre:"pre",a:"a",ol:"ol",li:"li",h3:"h3",ul:"ul"},i(),n.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(t.h1,{children:"Contributing to P.R.I.M.E"}),`
+import{M as i}from"./chunk-MA2MUXQN-bbbbfbe5.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{j as e}from"./jsx-runtime-c27a426b.js";import{u as p}from"./index-0e5a60ed.js";import"./preload-helper-101896b7.js";import"./index-c4c77e71.js";import"./_commonjsHelpers-725317a4.js";import"./index-55ae201a.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-3392a817.js";import"./index-d475d2ea.js";import"./memoizerific-cab73ecf.js";import"./index-014c75af.js";import"./chunk-XHUUYXNA-40ecb194.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-BVZGY62N-610dc239.js";import"./index-10d4cb7f.js";import"./_getTag-25edba2d.js";import"./index-842d733b.js";function l(n={}){const{wrapper:r}=Object.assign({},p(),n.components);return r?e.exports.jsx(r,Object.assign({},n,{children:e.exports.jsx(s,{})})):s();function s(){const t=Object.assign({h1:"h1",p:"p",code:"code",h2:"h2",pre:"pre",a:"a",ol:"ol",li:"li",h3:"h3",ul:"ul"},p(),n.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(t.h1,{children:"Contributing to P.R.I.M.E"}),`
 `,e.exports.jsxs(t.p,{children:["Please follow these instructions for contributing to ",e.exports.jsx(t.code,{children:"PRIME"}),"."]}),`
 `,e.exports.jsx(t.h2,{children:"Getting started"}),`
 `,e.exports.jsxs(t.p,{children:["To run the ",e.exports.jsx(t.code,{children:"Storybook"})," page locally, pull down the project and run:"]}),`
@@ -26,12 +26,11 @@ npm run storybook
 `,e.exports.jsx(t.h2,{children:"Development tasks"}),`
 `,e.exports.jsx(t.p,{children:"To verify that static assets build correctly:"}),`
 `,e.exports.jsx(t.pre,{children:e.exports.jsx(t.code,{className:"language-shell",children:`npm run build-storybook
+npm run build
 `})}),`
 `,e.exports.jsxs(t.p,{children:["To run tests locally, you must first install the ",e.exports.jsx(t.code,{children:"playground"})," development dependencies and playwright. From there, you can run tests"]}),`
 `,e.exports.jsx(t.pre,{children:e.exports.jsx(t.code,{className:"language-shell",children:`# set up playground
-cd playground
-npm install
-cd ..
+npm --prefix=playground install
 npx playwright install --with-deps
 
 # run tests, with optional debugging
@@ -42,14 +41,11 @@ npm run test-dev
 `,e.exports.jsx(t.pre,{children:e.exports.jsx(t.code,{className:"language-shell",children:`# check types, a11y, and other component quality
 npm run check
 
-# autoformat code and documentation
+# autoformat code and other files
 npm run format
 
-# check formatting without writing files
-npm run format-check
-
-# lint for stylistic and correctness issues
+# lint for formatting and correctness issues
 npm run lint
-`})})]})}}function l(n={}){const{wrapper:s}=Object.assign({},i(),n.components);return s?e.exports.jsx(s,{...n,children:e.exports.jsx(r,{})}):r();function r(){return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(p,{title:"Docs/Contributing"}),`
-`,e.exports.jsx(c,{})]})}}const a=()=>{throw new Error("Docs-only story")};a.parameters={docsOnly:!0};const o={title:"Docs/Contributing",tags:["mdx"],includeStories:["__page"]};o.parameters=o.parameters||{};o.parameters.docs={...o.parameters.docs||{},page:l};const P=["__page"];export{P as __namedExportsOrder,a as __page,o as default};
-//# sourceMappingURL=contributing.stories-e5834dd5.js.map
+`})})]})}}function c(n={}){const{wrapper:r}=Object.assign({},p(),n.components);return r?e.exports.jsx(r,{...n,children:e.exports.jsx(s,{})}):s();function s(){return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(i,{title:"Docs/Contributing"}),`
+`,e.exports.jsx(l,{})]})}}const a=()=>{throw new Error("Docs-only story")};a.parameters={docsOnly:!0};const o={title:"Docs/Contributing",tags:["mdx"],includeStories:["__page"]};o.parameters=o.parameters||{};o.parameters.docs={...o.parameters.docs||{},page:c};const P=["__page"];export{P as __namedExportsOrder,a as __page,o as default};
+//# sourceMappingURL=contributing.stories-9fdba5a6.js.map
