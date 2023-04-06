c57d33ab4b2253b6fe673c3797fb519523508259
Micheal Parks <micheal.parks@outlook.com>
2023-04-06 12:43:11 -0400

    simplify some tests

--- tests/badge.spec.ts
+++ tests/badge.spec.ts
@@ -5,14 +5,14 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/badge-test.html');
 });
 
+test('Renders the correct text', async ({ page }) => {
+  await expect(page.getByText('Inactive')).toBeVisible();
+})
+
 test('Renders a gray badge if the variant is specified as gray', async ({
   page,
 }) => {
   const grayBadge = page.getByText('Inactive');
-
-  await expect(grayBadge).toBeVisible();
-  await expect(grayBadge).toHaveText('Inactive');
-
   await expect(grayBadge).toHaveCSS(
     'background-color',
     hexToRGB('disabled-bg')
@@ -24,10 +24,6 @@ test('Renders a gray badge if the variant is not specified', async ({
   page,
 }) => {
   const defaultBadge = page.getByText('Default');
-
-  await expect(defaultBadge).toBeVisible();
-  await expect(defaultBadge).toHaveText('Default');
-
   await expect(defaultBadge).toHaveCSS(
     'background-color',
     hexToRGB('disabled-bg')
@@ -39,10 +35,6 @@ test('Renders a green badge if the variant is specified as green', async ({
   page,
 }) => {
   const greenBadge = page.getByText('Go', { exact: true });
-
-  await expect(greenBadge).toBeVisible();
-  await expect(greenBadge).toHaveText('Go');
-
   await expect(greenBadge).toHaveCSS(
     'background-color',
     hexToRGB('success-bg')
@@ -54,10 +46,6 @@ test('Renders an orange badge if the variant is specified as orange', async ({
   page,
 }) => {
   const orangeBadge = page.getByText('Danger');
-
-  await expect(orangeBadge).toBeVisible();
-  await expect(orangeBadge).toHaveText('Danger');
-
   await expect(orangeBadge).toHaveCSS(
     'background-color',
     hexToRGB('warning-bg')
@@ -69,10 +57,6 @@ test('Renders a red badge if the variant is specified as red', async ({
   page,
 }) => {
   const redBadge = page.getByText('Unhealthy');
-
-  await expect(redBadge).toBeVisible();
-  await expect(redBadge).toHaveText('Unhealthy');
-
   await expect(redBadge).toHaveCSS('background-color', hexToRGB('danger-bg'));
   await expect(redBadge).toHaveCSS('color', hexToRGB('danger-fg'));
 });
