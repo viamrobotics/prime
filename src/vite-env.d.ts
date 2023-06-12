d52bb081b0be6bb7197a1851c598099b1f8a308e
Micheal Parks <micheal.parks@outlook.com>
2023-06-12 16:01:45 -0400

    add comment

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -8,6 +8,7 @@ interface HTMLElement {
   ): void;
 }
 
+// TODO(mp, 2023-06-12): svelte 3 web components are all or nothing, remove when migrating to svelte 4
 declare module '*.component' {
   import { SvelteComponent } from 'svelte';
   const component: typeof SvelteComponent;
