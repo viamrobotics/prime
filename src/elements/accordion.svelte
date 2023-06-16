083eb02191ba902197fba02eb954a626282218c0
Anjali Nainani <anjali.nainani@gmail.com>
2023-06-16 17:02:49 -0400

    APP-2050 - get accord to display

--- src/elements/accordion.svelte
+++ src/elements/accordion.svelte
@@ -6,15 +6,15 @@
   import { dispatcher } from '../lib/dispatch';
   import { htmlToBoolean } from '../lib/boolean';
 
-  export let sections: { title: string; content: string; open?: string }[] = [];
-
+  export let sectionsArr: { title: string; content: string; open?: string }[] = [];
+  export let sections: string = ''
   const dispatch = dispatcher();
 
   addStyles();
 
   let openSections: boolean[] = [];
-
-  $: openSections = sections.map(section => htmlToBoolean(section.open, 'open') ? true : false);
+  sectionsArr = JSON.parse(sections)
+  $: openSections = sectionsArr.map(section => htmlToBoolean(section.open, 'open') ? true : false);
 
   const toggleSection = (index: number) => {
     openSections[index] = !openSections[index];
@@ -29,7 +29,7 @@
 </script>
 
 <div class="relative w-full" on:keydown={(event) => handleKeyDown(event, -1)}>
-  {#each sections as section, index}
+  {#each sectionsArr as section, index}
     <div
       class="p-4 flex items-center justify-between text-default cursor-pointer accordion-header"
       on:click={() => toggleSection(index)}
