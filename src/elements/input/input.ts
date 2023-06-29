964d8e4b38476a7eca430747ce4aa5b157461bb4
emily hong <emily.hong@viam.com>
2023-06-29 11:47:52 -0400

    test focus override

--- src/elements/input/input.ts
+++ src/elements/input/input.ts
@@ -3,6 +3,9 @@ import './input.svelte';
 // https://github.com/sveltejs/svelte/issues/7596
 class Input extends (customElements.get('v-input-internal')!) {
   static override formAssociated = true;
+  override focus() {
+    this.shadowRoot?.querySelector('input')?.focus();
+  }
 }
 
 customElements.define('v-input', Input);
