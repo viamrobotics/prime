25483eb0c705086f3b933cada7bf493055765a56
Ethan Look <ethan.look@gmail.com>
2023-07-27 14:48:52 -0400

    APP-2283 Migrate Breadcrumbs to @viamrobotics/prime-core (#285)

---
+++ packages/core/src/lib/breadcrumbs.svelte
@@ -0,0 +1,37 @@
+<!--
+@component
+  
+A navigation aid that helps users understand the relationships between parents and children.
+
+```svelte
+<Breadcrumbs crumbs={['Dessert', 'Cookie']} />
+```
+-->
+<svelte:options immutable />
+
+<script lang="ts">
+/**
+ * A list of each part.
+ */
+export let crumbs: [string, ...string[]];
+</script>
+
+<div
+  class="inline-flex gap-2.5 rounded-full border border-medium bg-light px-3 py-px text-default"
+>
+  {#each crumbs as crumb, index (crumb)}
+    <small class="text-xs">
+      {crumb}
+    </small>
+    {#if index !== crumbs.length - 1}
+      <div>
+        <div
+          class="-mt-0.5 h-[69%] w-px -rotate-[30deg] border-l border-medium"
+        />
+        <div
+          class="-mt-0.5 h-[69%] w-px rotate-[30deg] border-l border-medium"
+        />
+      </div>
+    {/if}
+  {/each}
+</div>
