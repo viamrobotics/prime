54da5dc8a1fc50a78f39538b4dfc680fc2c414c3
Ethan Look <ethan.look@viam.com>
2023-07-28 09:42:09 -0400

    Updates from icons improvement

--- packages/core/src/lib/button.svelte
+++ packages/core/src/lib/button.svelte
@@ -24,8 +24,7 @@ export let variant:
   | 'danger'
   | 'outline-danger'
   | 'success'
-  | 'icon'
-  | 'icon-danger' = 'primary';
+  | 'icon' = 'primary';
 /** The text displayed. */
 export let label = '';
 /** The text that appears in a popup box when mouse is over the element. */
@@ -38,72 +37,51 @@ export let title = '';
 export let icon = '';
 /** The width of the button. */
 export let width: 'full' | 'default' = 'default';
-
-// TODO(APP-2290): Allow the user to specify an icon once migrated.
-/*
- * let fill = 'gray-6';
- * $: {
- *   switch (variant) {
- *     case 'inverse-primary':
- *     case 'danger':
- *     case 'success': {
- *       fill = 'white';
- *       break;
- *     }
- *     case 'outline-danger':
- *     case 'icon-danger': {
- *       fill = 'danger-dark';
- *       break;
- *     }
- *   }
- *
- *   if (disabled) {
- *     fill = 'disabled';
- *   }
- * }
- */
 </script>
 
 <button
   {type}
-  aria-label={variant === 'icon' || variant === 'icon-danger'
-    ? label
-    : undefined}
+  aria-label={variant === 'icon' ? label : undefined}
   aria-disabled={disabled}
   {disabled}
   {title}
   class={cx('whitespace-nowrap', 'mx-auto', {
     'w-full': width === 'full',
-    'h-[30px] w-[30px]': variant === 'icon' || variant === 'icon-danger',
-    'px-3': !icon && variant !== 'icon' && variant !== 'icon-danger',
-    'pl-2 pr-3': icon && variant !== 'icon' && variant !== 'icon-danger',
+    'h-[30px] w-[30px]': variant === 'icon',
+    'px-3': !icon && variant !== 'icon',
+    'pl-2 pr-3': icon && variant !== 'icon',
     'inline-flex items-center justify-center gap-1.5 border px-3 py-1.5 text-xs':
-      variant !== 'icon' && variant !== 'icon-danger',
+      variant !== 'icon',
     'border-light bg-light hover:border-medium hover:bg-medium active:bg-gray-2':
       variant === 'primary',
     'border-gray-9 bg-gray-9 text-white hover:border-black hover:bg-black active:bg-[#000]':
       variant === 'inverse-primary',
     'active-border-[rgba(0,0,0,0.08)] border-transparent hover:bg-[rgba(0,0,0,0.04)] active:bg-[rgba(0,0,0,0.08)]':
-      variant === 'ghost' || variant === 'icon' || variant === 'icon-danger',
+      variant === 'ghost' || variant === 'icon',
     'border-danger-dark bg-danger-dark text-white hover:bg-[#aa2a2b] active:bg-[#9e2728]':
       variant === 'danger',
     'border-success-dark bg-success-dark text-white': variant === 'success',
     'border-danger-medium bg-danger-light text-danger-dark hover:bg-[#f5dfdc] active:bg-[#f6d7d3]':
       variant === 'outline-danger',
-    'pointer-events-none select-none !border-disabled-light !bg-disabled-light text-disabled-dark':
+    '!border-disabled-light !bg-disabled-light text-disabled-dark pointer-events-none select-none':
       disabled,
   })}
   style={disabled ? '-webkit-user-select: none' : ''}
   on:click
 >
   <!--  TODO(APP-2290): Allow the user to specify an icon once migrated. -->
-  <!--
-    {#if icon} 
-      <Icon name={icon} {fill} />
-    {/if}
+  <!-- 
+  {#if icon}
+    <Icon
+      name={icon}
+      class={cx({
+        'text-gray-400': variant === 'primary',
+      })}
+    />
+  {/if}
   -->
 
-  {#if variant !== 'icon' && variant !== 'icon-danger'}
+  {#if variant !== 'icon'}
     <span>
       {label}
     </span>
