0a275f9f3884fe225d0e5e134f4f1f44ff3822f5
Micheal Parks <micheal.parks@outlook.com>
2023-08-01 11:40:13 -0400

    lint

--- packages/blocks/src/app.d.ts
+++ packages/blocks/src/app.d.ts
@@ -1,11 +1,15 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
+/*
+ * See https://kit.svelte.dev/docs/types#app
+ * for information about these interfaces
+ */
 declare global {
   namespace App {
-    // interface Error {}
-    // interface Locals {}
-    // interface PageData {}
-    // interface Platform {}
+    /*
+     * interface Error {}
+     * interface Locals {}
+     * interface PageData {}
+     * interface Platform {}
+     */
   }
 }
 
