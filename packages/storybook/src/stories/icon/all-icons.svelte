710447c1b8cd2d0d838c33f7f65b55426f9a80ae
mrloureed <mrloureed@gmail.com>
2023-08-03 18:05:09 -0400

    format

--- packages/storybook/src/stories/icon/all-icons.svelte
+++ packages/storybook/src/stories/icon/all-icons.svelte
@@ -4,12 +4,9 @@ import { Icon } from '@viamrobotics/prime-core';
 type Size = 'xs' | 'sm' | 'base' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl';
 
 export let size: Size;
-
 </script>
 
-
 <Icon
-  name='close'
+  name="close"
   {size}
 />
-
