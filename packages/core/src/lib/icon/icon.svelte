1121ad77571039c37a2f6bb511045ada9ada7645
mrloureed <mrloureed@gmail.com>
2023-08-03 13:36:34 -0400

    cleanup - code review

--- packages/core/src/lib/icon/icon.svelte
+++ packages/core/src/lib/icon/icon.svelte
@@ -15,24 +15,19 @@ A component that renders SVG icons from the @mdi/js package
 import cx from 'classnames';
 import { paths } from './icons';
 
-/**
- * Size options
- */
 type Size = 'xs' | 'sm' | 'base' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl';
+
+/**
+ * The size of the icon.
+ */
+export let size: Size = 'base';
 
 /**
  * The name of the icon.
  */
 export let name = '';
 
-/**
- * Boolean to verify that name exists.
- */
 const hasNameProperty = Object.hasOwn(paths, name);
-
-/**
- * The TailwindCSS sizes map.
- */
 const sizes: Record<Size, string> = {
   xs: 'w-3 h-3',
   sm: 'w-3.5 h-3.5',
@@ -43,11 +38,6 @@ const sizes: Record<Size, string> = {
   '3xl': 'w-8 h-8',
   '4xl': 'w-9 h-9',
 };
-
-/**
- * The size of the icon.
- */
-export let size: Size = 'base';
 </script>
 
 <p class="leading-3">
