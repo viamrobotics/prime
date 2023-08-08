d49e350c4e1d3beb9ba9c66078881f42120845eb
emily hong <emily.hong@viam.com>
2023-08-08 16:20:38 -0400

    migrate context menu

---
+++ packages/core/src/lib/context-menu/context-menu.spec.ts
@@ -0,0 +1,53 @@
+import { describe, it, expect, vi } from 'vitest';
+import { render, screen, fireEvent } from '@testing-library/svelte';
+import ContextMenuItem from './context-menu-item.svelte';
+import ContextMenuSeparator from './context-menu-separator.svelte';
+
+describe('Context menu', () => {
+  it('Renders context menu item', () => {
+    render(ContextMenuItem, { label: 'label' });
+    expect(screen.getByText('label')).toBeVisible();
+  });
+
+  it('Renders context menu item with icon', () => {
+    render(ContextMenuItem, { label: 'primary', icon: 'close' });
+    expect(screen.getByRole('img', { name: 'close icon' })).toBeVisible();
+  });
+
+  it('Renders context menu separator', () => {
+    render(ContextMenuSeparator);
+    expect(screen.getByRole('separator')).toBeVisible();
+  });
+
+  it('Renders style variants', () => {
+    render(ContextMenuItem, { label: 'label' });
+    expect(screen.getByText('label')).toHaveClass('text-default');
+
+    render(ContextMenuItem, { label: 'primary', variant: 'primary' });
+    expect(screen.getByText('primary')).toHaveClass('text-default');
+
+    render(ContextMenuItem, { label: 'danger', variant: 'danger' });
+    expect(screen.getByText('danger')).toHaveClass('text-danger-dark');
+  });
+
+  it('Dispatches select events', async () => {
+    const { component } = render(ContextMenuItem, {
+      label: 'select me!',
+      icon: 'arrow-up',
+    });
+    const onSelect = vi.fn();
+    component.$on('select', onSelect);
+
+    await fireEvent.click(screen.getByRole('menuitem'));
+    expect(onSelect).toHaveBeenCalledOnce();
+    expect(onSelect).toHaveBeenCalledWith(
+      expect.objectContaining({ detail: { value: 'select me!' } })
+    );
+
+    await fireEvent.click(screen.getByText('select me!'));
+    expect(onSelect).toHaveBeenCalledTimes(2);
+
+    await fireEvent.click(screen.getByRole('img', { name: 'arrow-up icon' }));
+    expect(onSelect).toHaveBeenCalledTimes(3);
+  });
+});
---
+++ packages/core/src/lib/context-menu/context-menu.svelte
@@ -0,0 +1,8 @@
+<svelte:options immutable />
+
+<div
+  role="menu"
+  class="max-w-xs border border-medium py-1 shadow-sm"
+>
+  <slot class="filter-none" />
+</div>
