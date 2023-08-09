92752b9e356a4c1d416f5f1481cbd5c3639469b0
DTCurrie <devintcurrie@gmail.com>
2023-08-09 12:45:29 -0400

    Add Input and TextInput

--- packages/core/src/lib/index.ts
+++ packages/core/src/lib/index.ts
@@ -5,8 +5,15 @@ export { default as Icon } from './icon/icon.svelte';
 export { default as Label } from './label.svelte';
 export { default as Pill } from './pill.svelte';
 export { default as Radio } from './radio.svelte';
+
 export {
   default as Tooltip,
   type TooltipLocation,
   type TooltipState,
 } from './tooltip.svelte';
+
+export { default as Input } from './input/input.svelte';
+export {
+  default as TextInput,
+  type TextInputTypes,
+} from './input/text-input.svelte';
