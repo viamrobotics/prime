d9082f5a323a97820013ddec70510036a3601377
Jason <mrloureed@gmail.com>
2023-08-10 16:42:48 -0400

    App 2284 core button icons (#317)

--- packages/core/src/lib/button.svelte
+++ packages/core/src/lib/button.svelte
@@ -11,6 +11,7 @@ For user triggered actions.
 
 <script lang="ts">
 import cx from 'classnames';
+import { Icon } from '$lib';
 
 /** Whether or not the button accepts clicks. */
 export let disabled = false;
@@ -24,15 +25,14 @@ export let variant:
   | 'danger'
   | 'outline-danger'
   | 'success'
-  | 'icon' = 'primary';
+  | 'icon'
+  | 'icon-danger' = 'primary';
 /** The text displayed. */
 export let label = '';
 /** The text that appears in a popup box when mouse is over the element. */
 export let title = '';
 /**
  * The icon shown in the button.
- *
- * TODO(APP-2290): Allow the user to specify an icon once migrated.
  */
 export let icon = '';
 /** The width of the button. */
@@ -41,50 +41,55 @@ export let width: 'full' | 'default' = 'default';
 
 <button
   {type}
-  aria-label={variant === 'icon' ? label : undefined}
+  aria-label={variant === 'icon' || variant === 'icon-danger'
+    ? label
+    : undefined}
   aria-disabled={disabled}
   {disabled}
   {title}
-  class={cx('mx-auto whitespace-nowrap', {
+  class={cx('whitespace-nowrap', {
     'flex w-full': width === 'full',
-    'inline-flex': width !== 'full' && variant !== 'icon',
+    'inline-flex':
+      width !== 'full' && variant !== 'icon' && variant !== 'icon-danger',
     'h-[30px] w-[30px] text-gray-6 hover:text-gray-7 active:text-gray-8':
       variant === 'icon',
-    'px-3': !icon && variant !== 'icon',
-    'pl-2 pr-3': icon && variant !== 'icon',
-    'items-center justify-center gap-1.5 border px-3 py-1.5 text-xs':
-      variant !== 'icon',
+    'h-[30px] w-[30px] text-gray-6 hover:text-danger-dark active:text-danger-dark':
+      variant === 'icon-danger',
+    'px-3': !icon && variant !== 'icon' && variant !== 'icon-danger',
+    'pl-2 pr-3': icon && variant !== 'icon' && variant !== 'icon-danger',
+    'inline-flex items-center justify-center gap-1.5 border px-3 py-1.5 text-xs':
+      variant !== 'icon' && variant !== 'icon-danger',
     'border-light bg-light hover:border-medium hover:bg-medium active:bg-gray-2':
       variant === 'primary',
     'border-gray-9 bg-gray-9 text-white hover:border-black hover:bg-black active:bg-[#000]':
       variant === 'inverse-primary',
     'active-border-[rgba(0,0,0,0.08)] border-transparent hover:bg-[rgba(0,0,0,0.04)] active:bg-[rgba(0,0,0,0.08)]':
-      variant === 'ghost' || variant === 'icon',
+      variant === 'ghost' || variant === 'icon' || variant === 'icon-danger',
     'border-danger-dark bg-danger-dark text-white hover:bg-[#aa2a2b] active:bg-[#9e2728]':
       variant === 'danger',
     'border-success-dark bg-success-dark text-white': variant === 'success',
     'border-danger-medium bg-danger-light text-danger-dark hover:bg-[#f5dfdc] active:bg-[#f6d7d3]':
       variant === 'outline-danger',
-    '!border-disabled-light !bg-disabled-light': disabled && variant !== 'icon',
+    '!border-disabled-light !bg-disabled-light':
+      disabled && variant !== 'icon' && variant !== 'icon-danger',
     'pointer-events-none select-none text-disabled-dark': disabled,
+    'mx-auto': true,
   })}
   style={disabled ? '-webkit-user-select: none' : ''}
   on:click
 >
-  <!--  TODO(APP-2290): Allow the user to specify an icon once migrated. -->
-  <!-- 
   {#if icon}
-    <Icon
-      name={icon}
+    <span
       class={cx({
         'text-gray-6': variant === 'primary',
-        'text-gray-4': isDisabled,
+        'text-gray-4': disabled,
       })}
-    />
+    >
+      <Icon name={icon} />
+    </span>
   {/if}
-  -->
 
-  {#if variant !== 'icon'}
+  {#if variant !== 'icon' && variant !== 'icon-danger'}
     <span>
       {label}
     </span>
