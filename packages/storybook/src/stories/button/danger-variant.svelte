a4eb1e8564e94ddd0cd01cafa4c0559c732a4e47
mrloureed <mrloureed@gmail.com>
2023-08-10 15:54:42 -0400

    Add icons to button and icon-danger variant

---
+++ packages/storybook/src/stories/button/danger-variant.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { Button } from '@viamrobotics/prime-core';
+</script>
+
+<span class="hover:text-danger-dark">
+  <Button
+    icon="close"
+    variant="icon"
+  />
+</span>
+
+<style>
+.hover\:text-danger-dark:hover {
+  --tw-text-opacity: 1;
+  color: rgb(190 53 54 / var(--tw-text-opacity)) !important;
+}
+</style>
