62ddf848782b26243b9adc447fd2a78b2b934c6d
Micheal Parks <micheal.parks@outlook.com>
2023-08-15 11:12:04 -0400

    initial

---
+++ packages/blocks/src/lib/navigation/plugins/compute-bounding.ts
@@ -0,0 +1,43 @@
+import * as THREE from 'three';
+import { injectPlugin } from '@threlte/core';
+import { boundingRadius } from '../stores';
+
+type Props = { computeBounding: string };
+
+export const computeBoundingPlugin = () =>
+  injectPlugin<Props>('computeBounding', ({ ref, props }) => {
+    let currentRef = ref as THREE.BufferGeometry;
+    let currentProps = props;
+
+    if (
+      !(currentRef instanceof THREE.BufferGeometry) ||
+      !currentProps.computeBounding
+    ) {
+      return;
+    }
+
+    const handleChange = () => {
+      currentRef.computeBoundingSphere();
+      const radius = currentRef.boundingSphere?.radius;
+
+      if (radius) {
+        boundingRadius[currentProps.computeBounding] = radius;
+      }
+    };
+
+    return {
+      onRefChange(nextRef: THREE.BufferGeometry) {
+        currentRef = nextRef;
+        handleChange();
+
+        return () => {
+          // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+          delete boundingRadius[currentProps.computeBounding];
+        };
+      },
+      onPropsChange(nextProps) {
+        currentProps = nextProps;
+        handleChange();
+      },
+    };
+  });
