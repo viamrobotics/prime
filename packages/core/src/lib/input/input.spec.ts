5f539e2499db11dbccf69ccf4ef006f41eec5370
Devin T. Currie <DTCurrie@users.noreply.github.com>
2023-08-15 11:26:59 -0400

    Add numeric and slider inputs (#320)
    Co-authored-by: Jason <mrloureed@gmail.com>

--- packages/core/src/lib/input/input.spec.ts
+++ packages/core/src/lib/input/input.spec.ts
@@ -42,10 +42,37 @@ describe('Input', () => {
       'h-[30px] w-full appearance-none border px-2 py-1.5 text-xs leading-tight outline-none'
     );
 
-    expect(input).toHaveClass(
-      'bg-disabled-light text-disabled-dark border-disabled-light'
-    );
-
+    expect(input).toHaveClass('bg-light border-none');
     expect(input).not.toHaveAttribute('aria-disabled');
+  });
+
+  it('Renders the input in the info state', () => {
+    render(Input, { placeholder: 'Enter your name', state: 'info' });
+
+    const iconWrapper = screen.getByRole('img').parentElement;
+    const iconTitle = screen.getByTitle('information icon');
+
+    expect(iconWrapper).toHaveClass('text-info-dark');
+    expect(iconTitle).toBeInTheDocument();
+  });
+
+  it('Renders the input in the warn state', () => {
+    render(Input, { placeholder: 'Enter your name', state: 'warn' });
+
+    const iconWrapper = screen.getByRole('img').parentElement;
+    const iconTitle = screen.getByTitle('alert icon');
+
+    expect(iconWrapper).toHaveClass('text-warning-bright');
+    expect(iconTitle).toBeInTheDocument();
+  });
+
+  it('Renders the input in the error state', () => {
+    render(Input, { placeholder: 'Enter your name', state: 'error' });
+
+    const iconWrapper = screen.getByRole('img').parentElement;
+    const iconTitle = screen.getByTitle('alert-circle icon');
+
+    expect(iconWrapper).toHaveClass('text-danger-dark');
+    expect(iconTitle).toBeInTheDocument();
   });
 });
