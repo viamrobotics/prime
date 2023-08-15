a4223cb075c7126ab107b5ce09a9cbeb89f7edf1
Micheal Parks <micheal.parks@outlook.com>
2023-08-15 15:35:44 -0400

    remove unused var

--- packages/core/src/lib/icon/icon.spec.ts
+++ packages/core/src/lib/icon/icon.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/svelte';
+import { render } from '@testing-library/svelte';
 import Icon from './icon.svelte';
 
 describe('Icon', () => {
