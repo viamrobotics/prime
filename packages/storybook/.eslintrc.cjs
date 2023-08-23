ab76091441083108a3948951e80f9f632d64bccb
Devin T. Currie <DTCurrie@users.noreply.github.com>
2023-08-23 11:39:32 -0400

    Add select (#332)

--- packages/storybook/.eslintrc.cjs
+++ packages/storybook/.eslintrc.cjs
@@ -2,7 +2,7 @@
 
 module.exports = {
   root: true,
-  extends: ['@viamrobotics/eslint-config'],
+  extends: ['@viamrobotics/eslint-config/svelte'],
   parserOptions: {
     project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname,
@@ -10,5 +10,11 @@ module.exports = {
   env: {
     browser: true,
     node: true,
+  },
+  rules: {
+    // @todo(mp APP-2482): move to js-config
+    'sonarjs/no-duplicate-string': 'off',
+    'no-undef-init': 'off',
+    'unicorn/no-useless-undefined': 'off',
   },
 };
