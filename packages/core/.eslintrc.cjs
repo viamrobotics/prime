d0ebc7ccf2087a981cb5a6001dd02e567cb2af7c
Michael Cousins <michael.cousins@viam.com>
2023-08-28 15:10:31 -0400

    fixup: disable crashing rule

--- packages/core/.eslintrc.cjs
+++ packages/core/.eslintrc.cjs
@@ -17,4 +17,14 @@ module.exports = {
     'no-undef-init': 'off',
     'unicorn/no-useless-undefined': 'off',
   },
+  overrides: [
+    {
+      files: '*.svelte',
+      rules: {
+        // TODO(mc, 2023-08-28): this rule is crashing with svelte actions
+        // investigate and fix once lint dependencies are updated
+        'sonarjs/no-unused-collection': 'off',
+      },
+    },
+  ],
 };
