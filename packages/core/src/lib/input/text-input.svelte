2bcd5d33a4d3882912b57efe565e3f37dca01be8
Micheal Parks <103450731+micheal-parks@users.noreply.github.com>
2023-08-31 17:49:24 -0400

    Add missing text input bindings (#351)

--- packages/core/src/lib/input/text-input.svelte
+++ packages/core/src/lib/input/text-input.svelte
@@ -21,11 +21,19 @@ import Input from './input.svelte';
 
 /** The input type */
 export let type: TextInputTypes | undefined = 'text';
+
+/** The value of the input, if any. */
+export let value: string | undefined = undefined;
+
+/** The HTML input element. */
+export let input: HTMLInputElement | undefined = undefined;
 </script>
 
 <Input
   {...$$restProps}
   {type}
+  bind:value={value}
+  bind:input={input}
   on:input
   on:keydown
 />
