cc890355792b9d1505b9804a4d196e5201859d81
Michael Cousins <michael.cousins@viam.com>
2023-09-18 12:16:36 -0400

    APP-2703: rewrite core notifications to be context based (#377)

--- packages/core/src/lib/notification/index.ts
+++ packages/core/src/lib/notification/index.ts
@@ -1,2 +1,2 @@
-export { notify } from './notify';
 export { default as NotificationContainer } from './notification-container.svelte';
+export { provideNotify, useNotify, type Notify } from './context';
