bc3408192d792b9d5268d302a3e9d1b14f367fff
Micheal Parks <micheal.parks@outlook.com>
2023-09-21 15:10:15 -0400

    add obstacle draw tools

--- packages/blocks/src/lib/navigation-map/components/obstacle.svelte
+++ packages/blocks/src/lib/navigation-map/components/obstacle.svelte
@@ -1,26 +1,113 @@
 <script lang="ts">
 import { theme } from '@viamrobotics/prime-core/theme';
 import * as THREE from 'three';
-import { T } from '@threlte/core';
-import type { Obstacle } from '$lib';
+import { T, createRawEventDispatcher } from '@threlte/core';
+import { useMapLibre, type Obstacle, useMapLibreEvent } from '$lib';
 import { view, hovered, obstacleNavItems } from '../stores';
 import AxesHelper from './axes-helper.svelte';
+import type { MapMouseEvent } from 'maplibre-gl';
 
 /** An obstacle to render. */
 export let obstacle: Obstacle;
 
+interface $$Events {
+  /** Fired when obstacles are created, destroyed, or edited. */
+  update: Obstacle;
+}
+
+const dispatch = createRawEventDispatcher<$$Events>()
+const { map } = useMapLibre()
+
 let material: THREE.MeshPhongMaterial;
+let selected: string | null = null
+
+let pointerdownTheta = 0
+let pointerdownRadius = 0
+let pointerdownLength = 0
+let pointerdownWidth = 0
+let pointerdownHeight = 0
+
+const pointermove = new THREE.Vector2()
+const pointerdown = new THREE.Vector2()
 
 const handleGeometryCreate = ({ ref }: { ref: THREE.BufferGeometry }) => {
   ref.rotateX(-Math.PI / 2);
 };
 
+const handleMouseMove = (event: MapMouseEvent) => {
+  if (selected === null) return
+
+  console.log(event.originalEvent)
+
+  // Rotate
+  if (event.originalEvent.metaKey) {
+    pointermove.set(event.point.x, event.point.y)
+    pointermove.sub(pointerdown)
+    obstacle.geometries[0]!.pose.orientationVector.th = pointerdownTheta + pointermove.y
+  
+  // Scale
+  } else if (event.originalEvent.altKey) {
+    pointermove.set(event.point.x, event.point.y)
+    pointermove.sub(pointerdown)
+
+    if (obstacle.geometries[0]!.type === 'sphere') {
+      obstacle.geometries[0]!.radius = pointerdownRadius - pointermove.y
+    } else if (obstacle.geometries[0]!.type === 'box') {
+      obstacle.geometries[0]!.length = pointerdownLength - pointermove.y
+      obstacle.geometries[0]!.width = pointerdownWidth - pointermove.y
+      obstacle.geometries[0]!.height = pointerdownHeight - pointermove.y
+    } else if (obstacle.geometries[0]!.type === 'capsule') {
+      obstacle.geometries[0]!.radius = pointerdownRadius - pointermove.y
+      obstacle.geometries[0]!.length = pointerdownLength - pointermove.y
+    }
+
+  // Transform
+  } else {
+    obstacle.location.lng = event.lngLat.lng
+    obstacle.location.lat = event.lngLat.lat
+  }
+
+  dispatch('update', obstacle);
+};
+
 $: name = obstacle.name;
+$: active = $hovered === name || selected === name
 $: (material as THREE.MeshPhongMaterial | undefined)?.color.set(
-  $hovered === obstacle.name
+  active
     ? theme.extend.colors['solar-power']
     : theme.extend.colors['power-wire']
 );
+
+$: if (selected) {
+  map.dragPan.disable();
+  map.on('mousemove', handleMouseMove);
+} else {
+  map.dragPan.enable();
+  map.off('mousemove', handleMouseMove);
+}
+
+useMapLibreEvent('mousedown', (event) => {
+  pointerdown.set(event.point.x, event.point.y)
+
+  const geometry = obstacle.geometries[0]!
+  pointerdownTheta = obstacle.geometries[0]!.pose.orientationVector.th
+  
+  if (geometry.type === 'sphere') {
+    pointerdownRadius = geometry.radius
+  } else if (geometry.type === 'box') {
+    pointerdownLength = geometry.length
+    pointerdownWidth = geometry.width
+    pointerdownHeight = geometry.height
+  } else if (geometry.type === 'capsule') {
+    pointerdownRadius = geometry.radius
+    pointerdownLength = geometry.length
+  }
+})
+
+useMapLibreEvent('mouseup', () => {
+  selected = null
+})
+
 </script>
 
 {#each obstacle.geometries as geometry, index (index)}
@@ -31,14 +118,16 @@ $: (material as THREE.MeshPhongMaterial | undefined)?.color.set(
     rotation.y={geometry.pose.orientationVector.th * THREE.MathUtils.DEG2RAD}
     on:pointerenter={() => ($hovered = name)}
     on:pointerleave={() => ($hovered = null)}
-    on:click={() =>
+    on:pointerdown={() => {
+      selected = name
       $obstacleNavItems[name]?.scrollIntoView({
         behavior: 'smooth',
         block: 'nearest',
-      })}
+      })
+    }}
   >
     {#if geometry.type === 'box'}
-      {#if $hovered === name}
+      {#if active}
         <AxesHelper
           thickness={Math.max(
             geometry.length,
@@ -64,7 +153,7 @@ $: (material as THREE.MeshPhongMaterial | undefined)?.color.set(
         />
       {/if}
     {:else if geometry.type === 'sphere'}
-      {#if $hovered === name}
+      {#if active}
         <AxesHelper
           thickness={geometry.radius / 100}
           length={geometry.radius * 2}
@@ -85,7 +174,7 @@ $: (material as THREE.MeshPhongMaterial | undefined)?.color.set(
         />
       {/if}
     {:else if geometry.type === 'capsule'}
-      {#if $hovered === name}
+      {#if active}
         <AxesHelper
           thickness={Math.max(geometry.radius, geometry.length) / 100}
           length={Math.max(geometry.radius, geometry.length) * 2}
--- packages/blocks/src/lib/navigation-map/components/scene.svelte
+++ packages/blocks/src/lib/navigation-map/components/scene.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 import * as THREE from 'three';
 import { T, useThrelte } from '@threlte/core';
+import type { Obstacle } from '../types';
 import { view, obstacles } from '../stores';
 import { computeBoundingPlugin } from '../plugins/compute-bounding';
 import { renderPlugin } from '../plugins/render';
 import { interactivityPlugin } from '../plugins/interactivity';
 import ObstacleGeometries from './obstacle.svelte';
+import { createObstacle } from '../lib/create-obstacle';
+import { createName } from '../lib/create-name';
+
+import Drawtool from './drawtool.svelte';
 
 renderPlugin();
 computeBoundingPlugin();
@@ -13,6 +18,23 @@ interactivityPlugin();
 
 const { renderer } = useThrelte();
 const clippingPlane = new THREE.Plane(new THREE.Vector3(0, 1, 0), 0);
+
+const handleUpdate = (obstacle: Obstacle) => {
+
+}
+
+const handleDraw = ({ width, height, center }: { width: number; height: number; center: LngLat }) => {
+  const names = $obstacles.map((obstacle) => obstacle.name);
+  const name = createName(names, 'obstacle', $obstacles.length);
+  const obstacle = createObstacle(name, center)
+  if (obstacle.geometries[0]!.type === 'box') {
+    obstacle.geometries[0]!.length = width, 
+    obstacle.geometries[0]!.width = height
+  }
+  $obstacles = [obstacle, ...$obstacles];
+
+  // dispatch('update', $obstacles);
+}
 
 $: flat = $view === '2D';
 
@@ -27,5 +49,9 @@ $: renderer.clippingPlanes = flat ? [] : [clippingPlane];
 {/if}
 
 {#each $obstacles as obstacle (obstacle.name)}
-  <ObstacleGeometries {obstacle} />
+  <ObstacleGeometries {obstacle} on:update={handleUpdate} />
 {/each}
+
+<Drawtool
+  on:update={handleDraw}
+/>
