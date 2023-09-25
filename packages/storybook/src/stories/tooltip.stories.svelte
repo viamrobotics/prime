6ef04f07b1bb3e9e235c259f3e9aa3e427d0309f
Mike Cousins <mike@cousins.io>
2023-09-25 12:56:58 -0400

    fixup: add storybook

--- packages/storybook/src/stories/tooltip.stories.svelte
+++ packages/storybook/src/stories/tooltip.stories.svelte
@@ -1,30 +1,58 @@
 <script lang="ts">
 import { Meta, Story } from '@storybook/addon-svelte-csf';
-import { Tooltip, Icon } from '@viamrobotics/prime-core';
+import {
+  Tooltip,
+  TooltipContainer,
+  TooltipTarget,
+  TooltipText,
+  Icon,
+} from '@viamrobotics/prime-core';
 </script>
 
 <Meta title="Elements/Tooltip" />
 
 <Story name="Plain Text">
-  <div class="pt-5">
+  <div class="flex pt-5">
     <Tooltip let:tooltipID>
       <p aria-describedby={tooltipID}>This element has a tooltip.</p>
-      <Icon name="information-outline" />
       <p slot="description">This is the tooltip text!</p>
     </Tooltip>
   </div>
 </Story>
 
 <Story name="Rich HTML">
-  <div class="pt-5">
+  <div class="flex pt-5">
     <Tooltip let:tooltipID>
       <p aria-describedby={tooltipID}>
         This <b>element</b> has a <em>tooltip</em>.
       </p>
-      <Icon name="information-outline" />
       <p slot="description">
         This is the <b class="underline">tooltip text!</b>
       </p>
     </Tooltip>
+  </div>
+</Story>
+
+<Story name="Custom Target">
+  <div class="flex">
+    <TooltipContainer let:tooltipID>
+      <p
+        aria-describedby={tooltipID}
+        class="flex items-center gap-1"
+      >
+        This element has a tooltip on the icon.
+        <TooltipTarget>
+          <Icon
+            name="information-outline"
+            label="information"
+            cx="cursor-pointer"
+          />
+        </TooltipTarget>
+      </p>
+
+      <TooltipText location="right">
+        This is the <b class="underline">tooltip text!</b>
+      </TooltipText>
+    </TooltipContainer>
   </div>
 </Story>
