7c5aec7c23e0f4da1f165140c66ad4b40d4e073d
Micheal Parks <micheal.parks@outlook.com>
2023-10-09 17:48:26 -0400

    cleanup

--- packages/blocks/src/routes/+page.svelte
+++ packages/blocks/src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-import * as THREE from 'three'
 import pointcloudUrl from '$lib/assets/pointcloud.pcd?url';
 import motionPath from '$lib/assets/cbirrtsmooth800.txt?raw';
 import { MapLibre, SlamMap2D } from '$lib';
@@ -10,16 +9,6 @@ const fetchPointcloud = async () => {
   const buffer = await response.arrayBuffer();
   return new Uint8Array(buffer);
 };
-
-let basePose = { x: 0, y: 0, theta: 0 }
-
-setInterval(() => {
-  basePose = {
-    x: (Math.random() - 0.5) * 100,
-    y: (Math.random() - 0.5) * 100,
-    theta: Math.random() * 360
-  }
-}, 1000)
 </script>
 
 <div class="m-auto flex max-w-6xl flex-col gap-6 py-6">
@@ -29,7 +18,6 @@ setInterval(() => {
     <div class="relative aspect-video w-full border border-gray-200">
       {#await fetchPointcloud() then pointcloud}
         <SlamMap2D
-          {basePose}
           {pointcloud}
           {motionPath}
         />
