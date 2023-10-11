9ec43a7e8d83fde8bef7658a4b913b77a352ea88
Ethan Look <ethan.look@viam.com>
2023-10-11 10:37:30 -0400

    fixup: lint

--- packages/core/src/lib/input/index.ts
+++ packages/core/src/lib/input/index.ts
@@ -2,8 +2,5 @@ export { default as Input, type InputState } from './input.svelte';
 export { default as NumericInput } from './numeric-input.svelte';
 export { default as SliderInput } from './slider-input.svelte';
 export { type NumericInputTypes } from './utils';
-export {
-  default as TextInput,
-  type TextInputTypes,
-} from './text-input.svelte';
+export { default as TextInput, type TextInputTypes } from './text-input.svelte';
 export { default as RestrictedTextInput } from './restricted-text-input.svelte';
