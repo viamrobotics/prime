5dedf0a9cdf51e59b1008697d6e67894c1808e43
DTCurrie <devintcurrie@gmail.com>
2023-10-18 13:26:35 -0400

    Remove redundant test component doc strings and add blurb to README

--- packages/core/src/lib/__tests__/radio.spec.svelte
+++ packages/core/src/lib/__tests__/radio.spec.svelte
@@ -1,15 +1,3 @@
-<!-- 
-  @component
-
-  This component allows us to render a Notify with its slotted
-  children, due to limitations with rendering slots using
-  `@testing-library`.
-
-  See:
-  - https://sveltesociety.dev/recipes/testing-and-debugging/unit-testing-svelte-component
-  - https://github.com/testing-library/svelte-testing-library/issues/48
- -->
-
 <script lang="ts">
 import Radio from '$lib/radio.svelte';
 
