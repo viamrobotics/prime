96a9cebb0c1a712bb16c4357e8790f95f7e89692
DTCurrie <devintcurrie@gmail.com>
2023-10-19 12:14:30 -0400

    remove test component doc string

--- packages/core/src/lib/__tests__/code-snippet.spec.svelte
+++ packages/core/src/lib/__tests__/code-snippet.spec.svelte
@@ -1,14 +1,3 @@
-<!-- 
-  @component
-
-  This component allows us to render a CodeSnippet with its slotted
-  children, due to limitations with rendering slots using
-  `@testing-library`.
-
-  See:
-  - https://sveltesociety.dev/recipes/testing-and-debugging/unit-testing-svelte-component
-  - https://github.com/testing-library/svelte-testing-library/issues/48
- -->
 <script lang="ts">
 import CodeSnippet from '$lib/code-snippet.svelte';
 </script>
