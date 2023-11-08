b25d201ab900a72e82a356e42d0c86422a87a9c2
Michael Cousins <michael.cousins@viam.com>
2023-11-08 12:37:44 -0500

    fixup: fix modal storybook

--- packages/storybook/src/stories/modal.stories.svelte
+++ packages/storybook/src/stories/modal.stories.svelte
@@ -1,53 +1,64 @@
 <script lang="ts">
 import { Meta, Story } from '@storybook/addon-svelte-csf';
 import { Modal, Button } from '@viamrobotics/prime-core';
+
+let defaultModalOpen = false;
+let smallModalOpen = false;
 </script>
 
 <Meta title="Elements/Modal" />
 
 <Story name="Default">
-  <Modal
-    isOpen={true}
-    on:close={() => console.log('Default modal close')}
-  >
-    <svelte:fragment slot="title">Default title</svelte:fragment>
-    <svelte:fragment slot="message">This is the default modal.</svelte:fragment>
-    <Button
-      slot="primary"
-      on:click={() => console.log('Primary button clicked')}
+  <div class="h-[500px]">
+    <Button on:click={() => (defaultModalOpen = true)}>Open</Button>
+    <Modal
+      isOpen={defaultModalOpen}
+      on:close={() => (defaultModalOpen = false)}
     >
-      Primary
-    </Button>
-    <Button
-      slot="secondary"
-      variant="dark"
-      on:click={() => console.log('Secondary button clicked')}
-    >
-      Secondary
-    </Button>
-  </Modal>
+      <svelte:fragment slot="title">Default title</svelte:fragment>
+      <svelte:fragment slot="message"
+        >This is the default modal.</svelte:fragment
+      >
+      <Button
+        slot="primary"
+        on:click={() => console.log('Primary button clicked')}
+      >
+        Primary
+      </Button>
+      <Button
+        slot="secondary"
+        variant="dark"
+        on:click={() => console.log('Secondary button clicked')}
+      >
+        Secondary
+      </Button>
+    </Modal>
+  </div>
 </Story>
 
 <Story name="Small">
-  <Modal
-    isOpen={true}
-    variant="small"
-    on:close={() => console.log('Small modal close')}
-  >
-    <svelte:fragment slot="title">Small modal title</svelte:fragment>
-    <svelte:fragment slot="message">This is a small modal.</svelte:fragment>
-    <Button
-      slot="primary"
-      on:click={() => console.log('Primary button clicked')}
+  <div class="h-[200px]">
+    <Button on:click={() => (smallModalOpen = true)}>Open</Button>
+    <Modal
+      isOpen={smallModalOpen}
+      variant="small"
+      on:close={() => (smallModalOpen = false)}
     >
-      Primary
-    </Button>
-    <Button
-      slot="secondary"
-      variant="dark"
-      on:click={() => console.log('Secondary button clicked')}
-    >
-      Secondary
-    </Button>
-  </Modal>
+      <svelte:fragment slot="title">Small modal title</svelte:fragment>
+      <svelte:fragment slot="message">This is a small modal.</svelte:fragment>
+      <Button
+        slot="primary"
+        on:click={() => console.log('Primary button clicked')}
+      >
+        Primary
+      </Button>
+      <Button
+        slot="secondary"
+        variant="dark"
+        on:click={() => console.log('Secondary button clicked')}
+      >
+        Secondary
+      </Button>
+    </Modal>
+  </div>
 </Story>
