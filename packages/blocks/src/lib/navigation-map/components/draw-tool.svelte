107ae0458c701a05336dfc02da805f04c91a4692
Micheal Parks <103450731+micheal-parks@users.noreply.github.com>
2023-11-20 12:58:40 -0800

    Three.js related dependency upgrade (#432)

--- packages/blocks/src/lib/navigation-map/components/draw-tool.svelte
+++ packages/blocks/src/lib/navigation-map/components/draw-tool.svelte
@@ -3,7 +3,7 @@
   Renders an in-progress rectangle that represents a drawn area by the user.
 -->
 <script lang="ts">
-import * as THREE from 'three';
+import type * as THREE from 'three';
 import { T, createRawEventDispatcher } from '@threlte/core';
 import { useMapLibreEvent, useMapLibre } from '$lib';
 import {
@@ -128,6 +128,6 @@ $: if (drawing) {
         on:create={handleGeometryCreate}
       />
     {/if}
-    <T is={new THREE.MeshPhongMaterial({ color: 'red' })} />
+    <T.MeshPhongMaterial color="red" />
   </T.Mesh>
 </T.Group>
