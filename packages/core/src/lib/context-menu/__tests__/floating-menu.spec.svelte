4906e5f1813cfec29df741a06e0c90fc61d2d6c9
Michael Cousins <michael.cousins@viam.com>
2023-11-29 14:35:17 -0500

    APP-3122: make floating menu controlled, add aria props

--- packages/core/src/lib/context-menu/__tests__/floating-menu.spec.svelte
+++ packages/core/src/lib/context-menu/__tests__/floating-menu.spec.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
 import FloatingMenu from '../floating-menu.svelte';
 import ContextMenuItem from '../context-menu-item.svelte';
+
+let isOpen = false;
+const handleChange = (nextIsOpen: boolean) => (isOpen = nextIsOpen);
 </script>
 
-<FloatingMenu>
-  <span
-    slot="control"
-    let:isOpen
-  >
+<FloatingMenu
+  {isOpen}
+  onChange={handleChange}
+>
+  <span slot="control">
     {isOpen ? 'close' : 'open'}
   </span>
-  <svelte:fragment
-    slot="items"
-    let:closeMenu
-  >
-    <ContextMenuItem on:click={closeMenu}>item</ContextMenuItem>
+  <svelte:fragment slot="items">
+    <ContextMenuItem on:click={() => (isOpen = false)}>item</ContextMenuItem>
   </svelte:fragment>
 </FloatingMenu>
 <span data-testid="outside-element" />
