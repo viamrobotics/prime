47d1190f56e55226bfa64d1b7766e1db265ef124
Ojima <ojima.abraham@viam.com>
2023-12-08 20:01:22 -0500

    Added storybook tutorial

---
+++ packages/storybook/src/stories/use-toast.stories.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import { Meta, Story } from '@storybook/addon-svelte-csf';
+    import {
+      Button,
+      ToastContainer,
+      provideToast,
+      useToast,
+    } from '@viamrobotics/prime-core';
+    
+    provideToast();
+    
+    const toast = useToast();
+    </script>
+    
+    <Meta title="APIs/useToast" />
+    
+    <Story name="Basic">
+      <div class="flex flex-col">
+        <ToastContainer />
+    
+        <div class="flex gap-4 pt-80">
+          <Button on:click={() => toast.neutral('Neutral message')}>
+            Neutral Toast
+          </Button>
+          <Button on:click={() => toast.upload('Upload message')}>
+            Upload Toast
+          </Button>
+          <Button
+            variant="success"
+            on:click={() => toast.success('Success message', true)}
+          >
+            Success Toast
+          </Button>
+        </div>
+      </div>
+    </Story>
+    
