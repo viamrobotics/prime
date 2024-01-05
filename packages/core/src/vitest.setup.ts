4863b900755a6bdb996a1db28cc26dc848238fe7
Mike Cousins <mike@cousins.io>
2024-01-05 13:00:14 -0500

    fixup: stubGlobal does not work cool cool cool

--- packages/core/src/vitest.setup.ts
+++ packages/core/src/vitest.setup.ts
@@ -1,43 +1,44 @@
-import { beforeAll, afterEach, vi } from 'vitest';
+import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/svelte';
 import '@testing-library/jest-dom/vitest';
 
-beforeAll(() => {
-  /**
-   * `PointerEvent` does not exist in `jsdom` so this polyfill is based off this
-   * comment on the PR to add it:
-   *
-   * https://github.com/jsdom/jsdom/pull/2666#issuecomment-691216178
-   */
-  vi.stubGlobal(
-    'PointerEvent',
-    class PointerEvent extends MouseEvent {
-      public height: number | undefined;
-      public isPrimary: boolean | undefined;
-      public pointerId: number | undefined;
-      public pointerType: string | undefined;
-      public pressure: number | undefined;
-      public tangentialPressure: number | undefined;
-      public tiltX: number | undefined;
-      public tiltY: number | undefined;
-      public twist: number | undefined;
-      public width: number | undefined;
+/**
+ * `PointerEvent` does not exist in `jsdom` so this polyfill is based off this
+ * comment on the PR to add it:
+ *
+ * https://github.com/jsdom/jsdom/pull/2666#issuecomment-691216178
+ */
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+if (!global.PointerEvent) {
+  class PointerEvent extends MouseEvent {
+    public height: number | undefined;
+    public isPrimary: boolean | undefined;
+    public pointerId: number | undefined;
+    public pointerType: string | undefined;
+    public pressure: number | undefined;
+    public tangentialPressure: number | undefined;
+    public tiltX: number | undefined;
+    public tiltY: number | undefined;
+    public twist: number | undefined;
+    public width: number | undefined;
 
-      constructor(type: string, params: PointerEventInit = {}) {
-        super(type, params);
-        this.pointerId = params.pointerId;
-        this.width = params.width;
-        this.height = params.height;
-        this.pressure = params.pressure;
-        this.tangentialPressure = params.tangentialPressure;
-        this.tiltX = params.tiltX;
-        this.tiltY = params.tiltY;
-        this.pointerType = params.pointerType;
-        this.isPrimary = params.isPrimary;
-      }
+    constructor(type: string, params: PointerEventInit = {}) {
+      super(type, params);
+      this.pointerId = params.pointerId;
+      this.width = params.width;
+      this.height = params.height;
+      this.pressure = params.pressure;
+      this.tangentialPressure = params.tangentialPressure;
+      this.tiltX = params.tiltX;
+      this.tiltY = params.tiltY;
+      this.pointerType = params.pointerType;
+      this.isPrimary = params.isPrimary;
     }
-  );
-});
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
+  global.PointerEvent = PointerEvent as any;
+}
 
 afterEach(() => {
   cleanup();
