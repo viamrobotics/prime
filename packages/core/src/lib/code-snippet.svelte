1b23b1614370ecf97f29bba6983434fd292ab81c
mrloureed <mrloureed@gmail.com>
2024-02-08 11:29:06 -0500

    format

--- packages/core/src/lib/code-snippet.svelte
+++ packages/core/src/lib/code-snippet.svelte
@@ -155,7 +155,7 @@ onMount(async () => {
     <figcaption><slot name="caption" /></figcaption>
   {/if}
 
-  <div class="flex gap-x-4 bg-light p-2 items-baseline">
+  <div class="flex items-baseline gap-x-4 bg-light p-2">
     <!-- The formatting here is intentional to preserve the formatting of `code` -->
     <pre class="flex-1 overflow-x-auto language-{language}"><code
         bind:this={element}
