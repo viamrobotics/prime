086920a90c7181d88e85392071463fba7e7986ed
Micheal Parks <micheal.parks@outlook.com>
2024-04-18 12:00:44 -0400

    move styles to tailwind

--- packages/core/src/lib/button/progress.svelte
+++ packages/core/src/lib/button/progress.svelte
@@ -1,34 +1,23 @@
 <!-- @TODO(mp) Rip this out and make it our canonical loader. -->
-<div class="container">
+<div class="container -ml-1 flex w-4 items-center p-[1.5px]">
   {#each { length: 8 } as _, index}
     <div
-      style="transform: rotate({index * 45}deg); animation-delay: {index *
-        100}ms"
-      class="pill bg-gray-8"
+      style="
+        transform: rotate({index * 45}deg);
+        animation-delay: {index * 100}ms;
+      "
+      class="pill absolute -mt-[0.5px] h-px w-[3px] rounded-[1px] bg-gray-8"
     />
   {/each}
 </div>
 
 <style>
-.container {
-  display: flex;
-  align-items: center;
-  margin: 0 0;
-  width: 16px;
-  padding: 1.5px;
-  margin-left: -4px;
-}
 .pill {
-  position: absolute;
-  width: 3px;
-  height: 1px;
-  border-radius: 1px;
   transform-origin: 6.5px 0.5px;
-  margin-top: -0.5px;
-  animation: 0.8s linear 0s infinite fadeinout;
+  animation: 0.8s linear 0s infinite fadeout;
 }
 
-@keyframes fadeinout {
+@keyframes fadeout {
   0% {
     opacity: 1;
   }
