c6045f94e16bc9e5f760850c1b672b2a08a66601
Jalen Geason <dararahming@gmail.com>
2024-07-18 14:49:38 -0400

    APP-5544 - lint fixes

--- packages/core/src/lib/icon/icons.ts
+++ packages/core/src/lib/icon/icons.ts
@@ -86,7 +86,7 @@ export const paths: Record<string, IconPath> = {
   'language-python': MDI.mdiLanguagePython,
   'language-typescript': MDI.mdiLanguageTypescript,
   linkedin: MDI.mdiLinkedin,
-  'link': MDI.mdiLink,
+  link: MDI.mdiLink,
   'link-variant': MDI.mdiLinkVariant,
   linux: MDI.mdiLinux,
   lock: MDI.mdiLockOutline,
