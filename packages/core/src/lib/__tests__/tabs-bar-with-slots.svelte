f1b76eadd4f3c8139fa6426e617d1d79ffbbe059
mrloureed <mrloureed@gmail.com>
2024-08-05 11:49:31 -0400

    add tests for Tabs components

---
+++ packages/core/src/lib/__tests__/tabs-bar-with-slots.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+import TabsBar from '$lib/tabs-bar.svelte';
+import Tab from '$lib/tab.svelte';
+</script>
+
+<TabsBar variant="secondary">
+  <Tab
+    title="The first tab"
+    href="#first"
+    selected
+  ></Tab>
+  <Tab
+    title="The second tab"
+    href="#second"
+  ></Tab>
+  <Tab
+    title="The third tab"
+    href="#third"
+  ></Tab>
+</TabsBar>
