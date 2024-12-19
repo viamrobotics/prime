360671c62fa0173bb8d595a91e062cded3be408b
Micheal Parks <micheal.parks@outlook.com>
2024-12-19 14:00:44 -0500

    remove storybook

--- packages/storybook/svelte.config.js
+++
@@ -1,8 +0,0 @@
-import { vitePreprocess } from '@sveltejs/kit/vite';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  preprocess: vitePreprocess(),
-};
-
-export default config;
