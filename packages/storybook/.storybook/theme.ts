6c4a7fef9334a2326c8f51dbf5ed3c03020fcc11
Micheal Parks <103450731+micheal-parks@users.noreply.github.com>
2024-12-20 15:14:24 -0500

    Remove Storybook (#612)

--- packages/storybook/.storybook/theme.ts
+++
@@ -1,7 +0,0 @@
-import { create } from '@storybook/theming';
-
-// @ts-expect-error: TODO(mc, 2023-03-23): are we missing `base` here?
-export default create({
-  brandImage: 'https://app.viam.com/static/images/viam-logo-white.svg',
-  appBg: '#333',
-});
