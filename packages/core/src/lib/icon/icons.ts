cc4d2085f1477577490cb656f5c10cb6b2b71d74
Katie Peters <katiepeters@users.noreply.github.com>
2025-01-30 16:29:52 -0500

    DATA-3600 Tweak checkbox path (#627)

--- packages/core/src/lib/icon/icons.ts
+++ packages/core/src/lib/icon/icons.ts
@@ -49,7 +49,7 @@ export const IconPathsByName = {
   check: MDI.mdiCheck,
   'checkbox-blank-outline': MDI.mdiCheckboxBlankOutline,
   'checkbox-indeterminate':
-    'M7 13h10v-2H7v2zm-2 8a1.4 1.4 0 01-1.4-1.4V4.4A1.4 1.4 0 015 3h14a1.4 1.4 0 011.4 1.4v15.2A1.4 1.4 0 0119 21H5z',
+    'M5 3H19C20.1 3 21 3.89 21 5V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V5C3 3.89 3.89 3 5 3ZM7 13V11H17V13H7Z',
   'checkbox-marked': MDI.mdiCheckboxMarked,
   'chevron-double-up': MDI.mdiChevronDoubleUp,
   'chevron-down': MDI.mdiChevronDown,
