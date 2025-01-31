892ca01563ecd3c9929e1fcb0e5badfee95cdaef
Joyce Jiang <76878583+joycexjiang@users.noreply.github.com>
2025-01-31 11:03:25 -0500

    forgot to delete unused files sry

--- packages/core/src/lib/index.ts
+++ packages/core/src/lib/index.ts
@@ -81,6 +81,3 @@ export { useTimeout } from './use-timeout';
 export { uniqueId } from './unique-id';
 export { default as VectorInput } from './vector-input.svelte';
 export { highlightCode } from './highlight-code';
-
-export { default as ListTable } from './new_table/list-table.svelte';
-export { default as ListTableCell } from './new_table/list-table-cell.svelte';
